59d849d13e5e150bc6aac9bd697dd07cce1cdd0c
Oliver Rosten <oliver.rosten@gmail.com>
2018-09-20 22:10:26 +0100

    Code moved to cpp

--- UnitTests/Maths/Graph/GraphTesterDiagnostics.hpp
+++ UnitTests/Maths/Graph/GraphTesterDiagnostics.hpp
@@ -23,7 +23,7 @@ namespace sequoia::unit_testing
     template <class, template<class> class> class EdgeStorageTraits,
     template <class, template<class> class, bool> class NodeWeightStorageTraits
   >
-  class generic_graph_false_positives
+  class dynamic_graph_false_positives
     : public graph_operations<GraphFlavour, EdgeWeight, NodeWeight, EdgeWeightPooling, NodeWeightPooling, EdgeStorageTraits, NodeWeightStorageTraits, unit_test_logger<test_mode::false_positive>>
   {
   public:
@@ -31,31 +31,12 @@ namespace sequoia::unit_testing
   private:
     using base_t = graph_operations<GraphFlavour, EdgeWeight, NodeWeight, EdgeWeightPooling, NodeWeightPooling, EdgeStorageTraits, NodeWeightStorageTraits>;
       
-    using GGraph = typename base_t::graph_type;
+    using graph_t = typename base_t::graph_type;
 
     using graph_checker<unit_test_logger<test_mode::false_positive>>::check_equality;      
     using graph_checker<unit_test_logger<test_mode::false_positive>>::check_exception_thrown;
     using graph_checker<unit_test_logger<test_mode::false_positive>>::check_graph;
 
-    void execute_operations() override
-    {
-      using namespace maths;
-
-      using Edge = edge<EdgeWeight, utilities::protective_wrapper<EdgeWeight>>;     
-      using E_Edge = embedded_edge<EdgeWeight, data_sharing::independent, utilities::protective_wrapper<EdgeWeight>>;
-
-      GGraph network{};
-
-      if constexpr (mutual_info(GraphFlavour))
-      {
-        check_graph(network, {{}}, {}, LINE("Check false positive: empty graph versus single node"));
-        check_graph(network, {{E_Edge{0,0,0}, E_Edge{0,0,1}}}, {}, LINE("Check false positive: empty graph versus single node with loop"));
-      }
-      else
-      {
-        check_graph(network, {{}}, {}, LINE("Check false positive: empty graph versus single node"));
-        check_graph(network, {{Edge{0,0}}}, {}, LINE("Check false positive: empty graph versus single node with loop"));        
-      }
-    }
+    void execute_operations() override;
   };
 }
