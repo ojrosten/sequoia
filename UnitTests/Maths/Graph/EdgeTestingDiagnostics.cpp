dff2db7b172fa3e420575e47183ce2e50fd1d541
Oliver Rosten <oliver.rosten@gmail.com>
2018-10-30 14:57:15 +0000

    More diagnostic tests added.

--- UnitTests/Maths/Graph/EdgeTestingDiagnostics.cpp
+++ UnitTests/Maths/Graph/EdgeTestingDiagnostics.cpp
@@ -1,5 +1,7 @@
 #include "EdgeTestingDiagnostics.hpp"
 
+#include "SharingPolicies.hpp"
+
 namespace sequoia::unit_testing
 {
   void test_edge_false_positives::run_tests()
@@ -28,45 +30,81 @@ namespace sequoia::unit_testing
     using edge_t = partial_edge<null_weight, independent, utilities::protective_wrapper<null_weight>>;
 
     check_equality(edge_t{0}, edge_t{1}, LINE("Differing target indices"));
+
+    using compact_edge_t = partial_edge<null_weight, independent, utilities::protective_wrapper<null_weight>, unsigned char>;
+
+    check_equality(compact_edge_t{10}, compact_edge_t{255}, LINE("Differing target indices"));
   }
   
   void test_edge_false_positives::test_partial_edge_indep_weight()
   {
+    using namespace maths;
+    using namespace data_sharing;
+
+    using edge_t = partial_edge<int, independent, utilities::protective_wrapper<int>>;
+
+    check_equality(edge_t{0,0}, edge_t{1,0}, LINE("Differing targets, identical weights"));
+    check_equality(edge_t{0,0}, edge_t{0,1}, LINE("Identical targets, differeing weights"));
+    check_equality(edge_t{0,0}, edge_t{1,1}, LINE("Differing targets and weights"));    
   }
 
   void test_edge_false_positives::test_partial_edge_shared_weight()
   {
+    using namespace maths;
+    using namespace data_sharing;
+
+    using edge_t = partial_edge<int, shared, utilities::protective_wrapper<int>>;
+
+    check_equality(edge_t{0,0}, edge_t{1,0}, LINE("Differing targets, identical weights"));
+    check_equality(edge_t{0,0}, edge_t{0,1}, LINE("Identical targets, differeing weights"));
+    check_equality(edge_t{0,0}, edge_t{1,1}, LINE("Differing targets and weights"));    
   }
 
   void test_edge_false_positives::test_plain_embedded_partial_edge()
   {
+    using namespace maths;
+    using namespace data_sharing; 
   }
   
   void test_edge_false_positives::test_embedded_partial_edge_indep_weight()
   {
+    using namespace maths;
+    using namespace data_sharing; 
   }
   
   void test_edge_false_positives::test_embedded_partial_edge_shared_weight()
   {
+    using namespace maths;
+    using namespace data_sharing; 
   }
       
   void test_edge_false_positives::test_plain_edge()
   {
+    using namespace maths;
+    using namespace data_sharing; 
   }
   
   void test_edge_false_positives::test_weighted_edge()
   {
+    using namespace maths;
+    using namespace data_sharing; 
   }
 
   void test_edge_false_positives::test_plain_embedded_edge()
   {
+    using namespace maths;
+    using namespace data_sharing; 
   }
   
   void test_edge_false_positives::test_embedded_edge_indep_weight()
   {
+    using namespace maths;
+    using namespace data_sharing; 
   }
   
   void test_edge_false_positives::test_embedded_edge_shared_weight()
   {
+    using namespace maths;
+    using namespace data_sharing; 
   }
 }
