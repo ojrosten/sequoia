906f2fbf70867dd12a93a8d184dec9f69aeef12c
Oliver Rosten <oliver.rosten@gmail.com>
2019-01-13 09:37:32 +0000

    Templates for unit testing files.

---
+++ aux_files/CodeTemplates/MyClassTestingUtilities.hpp
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////
+//                 Copyright Oliver Rosten 2019.                  //
+// Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0. //
+//    (See accompanying file LICENSE.md or copy at                //
+//          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
+////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "UnitTestUtils.hpp"
+
+//#include "MyType.hpp"
+
+namespace sequoia::unit_testing
+{
+  //template<????>
+  struct details_checker<MyClass/*<????>*/>
+  {
+    using type = MyClass/*<????>*/;
+    
+    template<class Logger>
+    static void check(Logger& logger, const type& reference, const type& actual, std::string_view description="")
+    {
+      // TO DO
+    }
+  };
+}
