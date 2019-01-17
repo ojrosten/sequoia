d7b63b00a441150bb4ae41663da635c884e150af
Oliver Rosten <oliver.rosten@gmail.com>
2019-01-17 08:41:23 +0000

    Reference files added for testing purposes.

---
+++ aux_files/UnitTestCodeTemplates/ReferenceExamples/IteratorUtilities.hpp
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////
+//                 Copyright Oliver Rosten 2019.                  //
+// Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0. //
+//    (See accompanying file LICENSE.md or copy at                //
+//          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
+////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "UnitTestUtils.hpp"
+
+//#include "Iterator.hpp"
+
+namespace sequoia::unit_testing
+{
+  //template<???>
+  struct details_checker<utilities::iterator/*<???>*/>
+  {
+    using type = utilities::iterator/*<???>*/;
+    
+    template<class Logger>
+    static void check(Logger& logger, const type& reference, const type& actual, std::string_view description="")
+    {
+      // TO DO
+    }
+  };
+}
