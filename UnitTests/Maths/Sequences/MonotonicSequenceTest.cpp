6f2a74d9f0f2b9ae280375b268aac10c1ca32578
Oliver Rosten <oliver.rosten@gmail.com>
2019-03-27 20:22:26 +0000

    More testing

--- UnitTests/Maths/Sequences/MonotonicSequenceTest.cpp
+++ UnitTests/Maths/Sequences/MonotonicSequenceTest.cpp
@@ -22,10 +22,53 @@ namespace sequoia::unit_testing
   {
     using namespace sequoia::maths;
 
+    check_exception_thrown<std::logic_error>([](){ monotonic_sequence<int> s{1,2}; }, LINE("Invariant violated by initialization"));
+
     monotonic_sequence<int> s{}, t{1};
+    // - ; 1
+    
     check_equivalence(s, std::initializer_list<int>{}, LINE(""));
     check_equivalence(t, std::initializer_list<int>{1}, LINE(""));
     check_regular_semantics(s, t, LINE("Regular Semantics"));
+
+    check_exception_thrown<std::logic_error>([&t](){ t.push_back(2); });
+    check_equivalence(t, std::initializer_list<int>{1}, LINE("Invariant violated by attempted push_back"));
+
+    check_exception_thrown<std::logic_error>([&t](){ t.insert(t.cbegin(), 0); }, LINE("Invariant violated by attempted insertion at beginning"));
+    check_equivalence(t, std::initializer_list<int>{1}, LINE(""));
+
+    t.push_back(1);
+    // - ; 1,1
+
+    check_equivalence(t, std::initializer_list<int>{1, 1}, LINE(""));
+    check_equality(t, monotonic_sequence<int>{1,1}, LINE(""));
+
+    check_exception_thrown<std::logic_error>([&t](){ t.insert(t.cbegin() + 1, 2); }, LINE("Invariant violated by attempted insertion in middle"));
+
+    t.mutate(t.cbegin(), t.cend() - 1, [](int a){ return a *= 2; });
+    // - ; 2,1
+
+    check_equivalence(t, std::initializer_list<int>{2, 1}, LINE(""));
+    check_equality(t, monotonic_sequence<int>{2,1}, LINE(""));
+
+    s.mutate(s.begin(), s.end(), [](int a) { return a+1; });
+    check_equivalence(s, std::initializer_list<int>{}, LINE(""));
+
+    s.push_back(4);
+    // 4 ; 2,1
+
+    check_equivalence(s, std::initializer_list<int>{4}, LINE(""));
+    check_regular_semantics(s, t, LINE("Regular Semantics"));
+
+    t.erase(t.begin(), t.end());
+    // 4 ; -
+    
+    check_equivalence(t, std::initializer_list<int>{}, LINE(""));
+
+    s.erase(s.begin());
+    // - ; -
+
+    check_equivalence(s, std::initializer_list<int>{}, LINE(""));
   }
 
   void monotonic_sequence_test::test_static_decreasing_sequence()
