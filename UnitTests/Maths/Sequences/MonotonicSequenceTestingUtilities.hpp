1ecd71c55d1bb41e1f050620d9fd8f55ffa915f4
Oliver Rosten <oliver.rosten@gmail.com>
2019-03-27 08:37:13 +0000

    Corrections and improvements.

--- UnitTests/Maths/Sequences/MonotonicSequenceTestingUtilities.hpp
+++ UnitTests/Maths/Sequences/MonotonicSequenceTestingUtilities.hpp
@@ -23,36 +23,31 @@ namespace sequoia::unit_testing
     {
       if(check_equality(logger, reference.size(), actual.size(), impl::concat_messages(description, "Size incorrect")))
       {
-        if(!reference.empty())
-        {
-          auto i_ref{reference.begin()}, i_act{actual.begin()};
-          auto ci_ref{reference.begin()}, ci_act{actual.begin()};
-          auto ri_ref{reference.begin()}, ri_act{actual.begin()};
-          auto cri_ref{reference.begin()}, cri_act{actual.begin()};
+        auto i_ref{reference.begin()}, i_act{actual.begin()};
+        auto ci_ref{reference.cbegin()}, ci_act{actual.cbegin()};
+        auto ri_ref{reference.rbegin()}, ri_act{actual.rbegin()};
+        auto cri_ref{reference.crbegin()}, cri_act{actual.crbegin()};
 
-          for(;i_ref != reference.end(); ++i_ref, ++i_act, ++ci_ref, ++ci_act, ++ri_ref, ++ri_act, ++cri_ref, ++cri_act)
-          {          
-            using std::distance;
-            const auto i{distance(reference.begin(), i_ref)};
-            const std::string mess{" for index " + std::to_string(i)};
+        for(;i_ref != reference.end(); ++i_ref, ++i_act, ++ci_ref, ++ci_act, ++ri_ref, ++ri_act, ++cri_ref, ++cri_act)
+        {          
+          using std::distance;
+          const auto i{distance(reference.begin(), i_ref)};
+          const std::string mess{" for index " + std::to_string(i)};
           
-            check_equality(logger, *i_ref, *i_act, impl::concat_messages(description, "Dereferenced iterator wrong" + mess));
-            check_equality(logger, *ci_ref, *ci_act, impl::concat_messages(description, "Dereferenced citerator wrong" + mess));
+          check_equality(logger, *i_ref, *i_act, impl::concat_messages(description, "Dereferenced iterator wrong" + mess));
+          check_equality(logger, *ci_ref, *ci_act, impl::concat_messages(description, "Dereferenced citerator wrong" + mess));
 
-            check_equality(logger, reference[i], actual[i], impl::concat_messages(description, "operator[] wrong" + mess));  
+          check_equality(logger, reference[i], actual[i], impl::concat_messages(description, "operator[] wrong" + mess));  
 
-            const auto shift{static_cast<int64_t>(reference.size()) - i - 1};
-            check_equality(logger, *(ri_ref + shift), *(ri_act + shift), impl::concat_messages(description, "Dereferenced riterator wrong" + mess));
-            check_equality(logger, *(cri_ref + shift), *(cri_act + shift), impl::concat_messages(description, "Dereferenced criterator wrong" + mess));
-          }
+          const auto shift{static_cast<int64_t>(reference.size()) - i - 1};
+          check_equality(logger, *(ri_ref + shift), *(ri_act + shift), impl::concat_messages(description, "Dereferenced riterator wrong" + mess));
+          check_equality(logger, *(cri_ref + shift), *(cri_act + shift), impl::concat_messages(description, "Dereferenced criterator wrong" + mess));
         }
-        else
-        {
-          unit_testing::check(logger, reference.begin() == reference.end(), "iterator location wrong for empty sequence");
-          unit_testing::check(logger, reference.cbegin() == reference.cend(), "citerator location wrong for empty sequence");
-          unit_testing::check(logger, reference.rbegin() == reference.rend(), "riterator location wrong for empty sequence");
-          unit_testing::check(logger, reference.crbegin() == reference.crend(), "criterator location wrong for empty sequence");
-        }
+          
+        unit_testing::check(logger, i_ref == reference.end(), "iterator location wrong");
+        unit_testing::check(logger, ci_ref == reference.cend(), "citerator location wrong");
+        unit_testing::check(logger, ri_ref == reference.rend(), "riterator location wrong");
+        unit_testing::check(logger, cri_ref == reference.crend(), "criterator location wrong");
       }
     }
   };
