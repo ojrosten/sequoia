f05b3d4d13e95df3136f3797f1a418ea91b687ac
Oliver Rosten <oliver.rosten@gmail.com>
2019-04-15 21:24:30 +0100

    Conventions changed

--- UnitTests/Maths/Graph/EdgeTestingUtilities.hpp
+++ UnitTests/Maths/Graph/EdgeTestingUtilities.hpp
@@ -8,31 +8,31 @@ namespace sequoia::unit_testing
   namespace impl
   {
     template<class Logger, class Edge>
-    void check_partial(Logger& logger, const Edge& reference, const Edge& actual, std::string_view description)
+    void check_partial(Logger& logger, const Edge& edge, const Edge& prediction, std::string_view description)
     {
-      check_equality(logger, reference.target_node(), actual.target_node(),
+      check_equality(logger, edge.target_node(), prediction.target_node(),
                        impl::concat_messages(description, "Target node incorrect"));
     
       if constexpr (!std::is_empty_v<typename Edge::weight_type>)
       {
-        check_equality(logger, reference.weight(), actual.weight(), impl::concat_messages(description, "Weight incorrect"));
+        check_equality(logger, prediction.weight(), edge.weight(), impl::concat_messages(description, "Weight incorrect"));
       }
     }
 
     template<class Logger, class Edge>
-    void check_complementary(Logger& logger, const Edge& reference, const Edge& actual, std::string_view description)
+    void check_complementary(Logger& logger, const Edge& edge, const Edge& prediction, std::string_view description)
     {
-      check_equality(logger, reference.complementary_index(), actual.complementary_index(),
+      check_equality(logger, edge.complementary_index(), prediction.complementary_index(),
                      impl::concat_messages(description, "Complementary index incorrect"));
     }
   
     template<class Logger, class Edge>
-    void check_host(Logger& logger, const Edge& reference, const Edge& actual, std::string_view description)
+    void check_host(Logger& logger, const Edge& prediction, const Edge& edge, std::string_view description)
     {
-      check_equality(logger, reference.host_node(), actual.host_node(),
+      check_equality(logger, edge.host_node(), prediction.host_node(),
                          impl::concat_messages(description, "Host node incorrect"));
 
-      check_equality(logger, reference.inverted(), actual.inverted(),
+      check_equality(logger, edge.inverted(), prediction.inverted(),
                          impl::concat_messages(description, "Inversion flag incorrect")); 
 
     }
@@ -47,10 +47,12 @@ namespace sequoia::unit_testing
   >
   struct details_checker<maths::partial_edge<Weight, WeightSharingPolicy, WeightProxy, IndexType>>
   {
+    using type = maths::partial_edge<Weight, WeightSharingPolicy, WeightProxy, IndexType>;
+    
     template<class Logger>
-    static void check(Logger& logger, const maths::partial_edge<Weight, WeightSharingPolicy, WeightProxy, IndexType>& reference, const maths::partial_edge<Weight, WeightSharingPolicy, WeightProxy, IndexType>& actual, std::string_view description="")
+    static void check(Logger& logger, const type& prediction, const type& edge, std::string_view description="")
     {
-      impl::check_partial(logger, reference, actual, description);
+      impl::check_partial(logger, edge, prediction, description);
     }
   };
 
@@ -63,11 +65,12 @@ namespace sequoia::unit_testing
   >
   struct details_checker<maths::embedded_partial_edge<Weight, WeightSharingPolicy, WeightProxy, IndexType>>
   {
+    using type = maths::embedded_partial_edge<Weight, WeightSharingPolicy, WeightProxy, IndexType>;
     template<class Logger>
-    static void check(Logger& logger, const maths::embedded_partial_edge<Weight, WeightSharingPolicy, WeightProxy, IndexType>& reference, const maths::embedded_partial_edge<Weight, WeightSharingPolicy, WeightProxy, IndexType>& actual, std::string_view description="")
+    static void check(Logger& logger, const type& prediction, const type& edge, std::string_view description="")
     {
-      impl::check_partial(logger, reference, actual, description);
-      impl::check_complementary(logger, reference, actual, description);
+      impl::check_partial(logger, edge, prediction, description);
+      impl::check_complementary(logger, edge, prediction, description);
     }
   };
 
@@ -79,11 +82,13 @@ namespace sequoia::unit_testing
   >
   struct details_checker<maths::edge<Weight, WeightProxy, IndexType>>
   {
+    using type = maths::edge<Weight, WeightProxy, IndexType>;
+    
     template<class Logger>
-    static void check(Logger& logger, const maths::edge<Weight, WeightProxy, IndexType>& reference, const maths::edge<Weight, WeightProxy, IndexType>& actual, std::string_view description="")
+    static void check(Logger& logger, const type& edge, const type& prediction, std::string_view description="")
     {
-      impl::check_partial(logger, reference, actual, description);
-      impl::check_host(logger, reference, actual, description);
+      impl::check_partial(logger, edge, prediction, description);
+      impl::check_host(logger, edge, prediction, description);
     }
   };
 
@@ -96,12 +101,14 @@ namespace sequoia::unit_testing
   >
   struct details_checker<maths::embedded_edge<Weight, WeightSharingPolicy, WeightProxy, IndexType>>
   {
+    using type = maths::embedded_edge<Weight, WeightSharingPolicy, WeightProxy, IndexType>;
+    
     template<class Logger>
-    static void check(Logger& logger, const maths::embedded_edge<Weight, WeightSharingPolicy, WeightProxy, IndexType>& reference, const maths::embedded_edge<Weight, WeightSharingPolicy, WeightProxy, IndexType>& actual, std::string_view description="")
+    static void check(Logger& logger, const type& edge, const type& prediction, std::string_view description="")
     {
-      impl::check_partial(logger, reference, actual, description);
-      impl::check_complementary(logger, reference, actual, description);
-      impl::check_host(logger, reference, actual, description);
+      impl::check_partial(logger, edge, prediction, description);
+      impl::check_complementary(logger, edge, prediction, description);
+      impl::check_host(logger, edge, prediction, description);
     }
   };
 }
