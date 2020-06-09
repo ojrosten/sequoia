574665b95eb8892fb80e4ea0f17c4b3c65893608
Oliver Rosten <oliver.rosten@gmail.com>
2020-06-09 21:56:10 +0100

    Missing file

---
+++ TestFramework/AllocationCheckersDetails.cpp
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////
+//                 Copyright Oliver Rosten 2020.                  //
+// Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0. //
+//    (See accompanying file LICENSE.md or copy at                //
+//          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
+////////////////////////////////////////////////////////////////////
+
+/*! \file
+    \brief Definitions for AllocationCheckersDetails.hpp
+*/
+
+#include "AllocationCheckersDetails.hpp"
+
+namespace sequoia::testing::impl
+{
+  std::string allocation_advice::operator()(int count, int) const
+  {
+    return (count < 0)
+      ? "A negative allocation count generally indicates an allocator propagting when it should(not)."
+      : "";
+  }
+}
