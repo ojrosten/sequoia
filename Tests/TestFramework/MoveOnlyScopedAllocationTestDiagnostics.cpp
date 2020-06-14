23cfdfbccf5d74b48525b95e460f8f8da7609c49
Oliver Rosten <oliver.rosten@gmail.com>
2020-06-14 22:36:52 +0100

    False-positive testing

--- Tests/TestFramework/MoveOnlyScopedAllocationTestDiagnostics.cpp
+++ Tests/TestFramework/MoveOnlyScopedAllocationTestDiagnostics.cpp
@@ -78,5 +78,76 @@ namespace sequoia::testing
   template<bool PropagateMove, bool PropagateSwap>
   void move_only_scoped_allocation_false_positive_diagnostics::test_regular_semantics()
   {
+    using beast
+      = move_only_scoped_beast<shared_counting_allocator<char, true, PropagateMove, PropagateSwap>>;
+
+    auto mutator{
+      [](beast& b) {
+        b.x.push_back("something too long for small string optimization");
+      }
+    };
+
+    using allocator = typename beast::allocator_type;
+    using info = move_only_allocation_info<beast, allocator>;
+
+    auto allocGetter{
+      [](const beast& b) {
+        return b.x.get_allocator();
+      }
+    };
+
+    if constexpr(!PropagateMove)
+    {
+      check_semantics(LINE("Incorrect assigment outer allocs"),
+                      beast{},
+                      beast{{"something too long for small string optimization"}},
+                      beast{},
+                      beast{{"something too long for small string optimization"}},
+                      mutator, info{allocGetter, {{0_anp, 1_mu, 1_pm}, {1_anp, 1_mu, 1_pm}}}
+      );
+    }
+
+    check_semantics(LINE("Incorrect mutation outer allocs"),
+                    beast{},
+                    beast{{"something too long for small string optimization"}},
+                    beast{},
+                    beast{{"something too long for small string optimization"}},
+                    mutator, info{allocGetter, {{1_anp, 0_mu, 1_pm}, {1_anp, 1_mu, 1_pm}}}
+    );
+
+    check_semantics(LINE("Incorrect para move outer allocs"),
+                    beast{},
+                    beast{{"something too long for small string optimization"}},
+                    beast{},
+                    beast{{"something too long for small string optimization"}},
+                    mutator, info{allocGetter, {{1_anp, 1_mu, 0_pm}, {1_anp, 1_mu, 1_pm}}}
+    );
+
+    if constexpr(!PropagateMove)
+    {
+      check_semantics(LINE("Incorrect assigment inner allocs"),
+                      beast{},
+                      beast{{"something too long for small string optimization"}},
+                      beast{},
+                      beast{{"something too long for small string optimization"}},
+                      mutator, info{allocGetter, {{1_anp, 1_mu, 1_pm}, {0_anp, 1_mu, 1_pm}}}
+      );
+    }
+
+    check_semantics(LINE("Incorrect mutation inner allocs"),
+                    beast{},
+                    beast{{"something too long for small string optimization"}},
+                    beast{},
+                    beast{{"something too long for small string optimization"}},
+                    mutator, info{allocGetter, {{1_anp, 1_mu, 1_pm}, {1_anp, 0_mu, 1_pm}}}
+    );
+
+    check_semantics(LINE("Incorrect para move inner allocs"),
+                    beast{},
+                    beast{{"something too long for small string optimization"}},
+                    beast{},
+                    beast{{"something too long for small string optimization"}},
+                    mutator, info{allocGetter, {{1_anp, 1_mu, 1_pm}, {1_anp, 1_mu, 0_pm}}}
+    );
   }
 }
