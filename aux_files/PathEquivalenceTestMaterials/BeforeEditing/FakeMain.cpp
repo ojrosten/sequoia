52ea46240b80fbaf085f916b3e99f9e4f122c8fd
Oliver Rosten <oliver.rosten@gmail.com>
2020-09-10 21:23:49 +0100

    New test materials

---
+++ aux_files/PathEquivalenceTestMaterials/BeforeEditing/FakeMain.cpp
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////
+//                Copyright Oliver J. Rosten 2020.                //
+// Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0. //
+//    (See accompanying file LICENSE.md or copy at                //
+//          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
+////////////////////////////////////////////////////////////////////
+
+#include "Includes.hpp"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  using namespace sequoia;
+  using namespace testing;
+
+  try
+  {
+    runner.add_test_family(
+      "CommandLine Arguments",
+      commandline_arguments_test{"Unit Test"},
+      commandline_arguments_false_positive_test{"False Positive Test"}
+    );
+      
+    runner.execute();
+  }
+  catch(const std::exception& e)
+  {
+    std::cout << e.what();
+  }
+  catch(...)
+  {
+    std::cout << "Unrecognized error\n"; 
+  }
+  
+  return 0;
+}
