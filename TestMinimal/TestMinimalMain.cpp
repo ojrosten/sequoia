96be0dcee5fe42962e46d285be60e9140aae21cf
Oliver Rosten <oliver.rosten@gmail.com>
2020-10-11 09:17:41 +0100

    New project for faster iteration

---
+++ TestMinimal/TestMinimalMain.cpp
@@ -0,0 +1,59 @@
+////////////////////////////////////////////////////////////////////
+//                Copyright Oliver J. Rosten 2019.                //
+// Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0. //
+//    (See accompanying file LICENSE.md or copy at                //
+//          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
+////////////////////////////////////////////////////////////////////
+
+#include "TestIncludes.hpp"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  using namespace sequoia;
+  using namespace testing;
+
+  try
+  {
+    const auto root{project_root(argc, argv)};
+    test_runner runner{argc,
+                       argv,
+                       "Oliver J. Rosten",
+                       root/"TestChamber"/"TestChamberMain.cpp",
+                       root/"TestCommon" /"TestIncludes.hpp",
+                       repositories(root)
+    };
+  
+    runner.add_test_family(
+      "Diagnostics",
+      false_positive_diagnostics{"False Positive Diagnostics"},
+      false_negative_diagnostics{"False Negative Diagnostics"}
+    );
+
+    /*runner.add_test_family(
+      "CommandLine Arguments",      
+      commandline_arguments_false_positive_test{"False Positive Test"},
+      commandline_arguments_test{"Unit Test"}
+    );
+
+    runner.add_test_family(
+      "Test Runner",      
+      test_runner_false_positive_test{"False Positive Diagnostics"},
+      test_runner_test{"Unit Test"}
+      );*/
+      
+    runner.execute();
+  }
+  catch(const std::exception& e)
+  {
+    std::cout << e.what();
+  }
+  catch(...)
+  {
+    std::cout << "Unrecognized error\n"; 
+  }
+  
+  return 0;
+}
+
