4340b3197f15275496fcb1d78d052a71b159377c
Oliver Rosten <oliver.rosten@gmail.com>
2021-01-26 20:13:01 +0000

    Tests added

--- Tests/Runtime/FactoryTest.cpp
+++ Tests/Runtime/FactoryTest.cpp
@@ -7,6 +7,8 @@
 
 #include "FactoryTest.hpp"
 
+#include <complex>
+
 namespace sequoia::testing
 {
   [[nodiscard]]
@@ -19,7 +21,31 @@ namespace sequoia::testing
   {
     using namespace runtime;
 
-    factory<int, double> f{{"int", "double"}}, g{{"foo", "bar"}};
-    check_semantics(LINE(""), f, g);
+    {
+      using prediction_type = std::array<std::pair<std::string, std::variant<int, double>>, 2>;
+
+      factory<int, double> f{{"int", "double"}}, g{{"bar", "foo"}};
+
+      check_equivalence(LINE(""), f, prediction_type{{{"int", 0}, {"double", 0.0}}});
+      check_equivalence(LINE(""), g, prediction_type{{{"bar", 0}, {"foo", 0.0}}});
+
+      check_semantics(LINE(""), f, g);
+
+      check_exception_thrown<std::runtime_error>(LINE(""), [&f](){ return f.create("plurgh"); });
+    }
+
+    {
+      using prediction_type = std::array<std::pair<std::string, std::variant<std::vector<int>, int, std::complex<float>, double>>, 4>;
+      factory<std::vector<int>, int, std::complex<float>, double>
+        f{{"vec", "int", "complex", "double"}}, g{{"baz", "foo", "bar", "huh"}};
+
+      check_equivalence(LINE(""), f,
+                        prediction_type{{{"vec", std::vector<int>{}}, {"int", 0}, {"complex", std::complex<float>{}}, {"double", 0.0}}});
+
+      check_equivalence(LINE(""), g,
+                        prediction_type{{{"baz", std::vector<int>{}}, {"foo", 0}, {"bar", std::complex<float>{}}, {"huh", 0.0}}});
+
+      check_semantics(LINE(""), f, g);
+    }
   }
 }
