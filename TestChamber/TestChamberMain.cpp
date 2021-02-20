4bb9cec5dac9980bb5ffb6c002c3f5ff8a69e9ea
Oliver Rosten <oliver.rosten@gmail.com>
2021-02-20 15:18:31 +0000

    Renaming

--- TestChamber/TestChamberMain.cpp
+++ TestChamber/TestChamberMain.cpp
@@ -20,31 +20,10 @@ int main(int argc, char** argv)
     test_runner runner{argc,
                        argv,
                        "Oliver J. Rosten",
-                       root/"TestMinimal"/"TestMinimalMain.cpp",
+                       root/"TestChamber"/"TestChamberMain.cpp",
                        root/"TestCommon" /"TestIncludes.hpp",
                        repositories(root)
     };
-  
-    /*
-    runner.add_test_family(
-      "Core Diagnostics",
-      false_positive_diagnostics{"False Positive Diagnostics"},
-      false_negative_diagnostics{"False Negative Diagnostics"}
-    );
-
-    runner.add_test_family(
-      "Test Runner",      
-      test_runner_false_positive_test{"False Positive Diagnostics"},
-      test_runner_test_creation{"Test Creation"},
-      test_runner_project_creation{"Project Creation"}
-    );
-
-    runner.add_test_family(
-      "CommandLine Arguments",      
-      commandline_arguments_false_positive_test{"False Positive Test"},
-      commandline_arguments_test{"Unit Test"}
-      );
-    */
       
     runner.execute();
   }
