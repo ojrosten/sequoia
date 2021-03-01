836de800ad4b28e65951e3daafb473379daaabfc
Oliver Rosten <oliver.rosten@gmail.com>
2021-03-01 20:20:22 +0000

    Skeleton of new test suite

---
+++ TestFrameworkHarness/TestFrameworkHarnessMain.cpp
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////
+//                Copyright Oliver J. Rosten 2019.                //
+// Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0. //
+//    (See accompanying file LICENSE.md or copy at                //
+//          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
+////////////////////////////////////////////////////////////////////
+
+#include "TestIncludes.hpp"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  using namespace sequoia;
+  using namespace testing;
+
+  try
+  {
+    const auto root{project_root(argc, argv)};
+    test_runner runner{argc,
+                       argv,
+                       "Oliver J. Rosten",
+                       root/"TestChamber"/"TestChamberMain.cpp",
+                       root/"TestCommon" /"TestIncludes.hpp",
+                       repositories(root)
+    };
+      
+    runner.execute();
+  }
+  catch(const std::exception& e)
+  {
+    std::cout << e.what();
+  }
+  catch(...)
+  {
+    std::cout << "Unrecognized error\n"; 
+  }
+  
+  return 0;
+}
+
