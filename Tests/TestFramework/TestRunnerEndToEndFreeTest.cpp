83c6487a9194b583fb2957d041263487870855c8
Oliver Rosten <oliver.rosten@gmail.com>
2021-03-09 21:17:31 +0000

    Further end-to-end testing

--- Tests/TestFramework/TestRunnerEndToEndFreeTest.cpp
+++ Tests/TestFramework/TestRunnerEndToEndFreeTest.cpp
@@ -98,7 +98,8 @@ namespace sequoia::testing
       return run_cmd().append(" create free_test Utilities.hpp"
                               " create regular_test \"other::functional::maybe<class T>\" \"std::optional<T>\""
                               " create move_only_test \"bar::baz::foo<maths::floating_point T>\" T"
-                              " create regular_allocation_test container");
+                              " create regular_allocation_test container"
+                              " create performance_test Container.hpp");
     }
   }
 
