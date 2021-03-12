7c60b4f06f75d5c92fa945df488508241c1bfd6a
Oliver Rosten <oliver.rosten@gmail.com>
2021-03-12 17:38:53 +0000

    Build system updates

--- build_system/Utilities.cmake
+++ build_system/Utilities.cmake
@@ -4,8 +4,7 @@ include(${CURRENT_DIR}/Globbing.cmake)
 
 FUNCTION(ADD_TEST_INCLUDE_DIRS target)
     HEADER_DIRECTORIES(testHeaderList ../Tests/*.hpp)
-    target_include_directories(${target} PUBLIC ${testHeaderList})
-    target_include_directories(${target} PUBLIC ../TestCommon)
+    target_include_directories(${target} PRIVATE ${testHeaderList})
 ENDFUNCTION()
 
 FUNCTION(LINK_LIBRARIES target)
@@ -16,9 +15,23 @@ FUNCTION(LINK_LIBRARIES target)
     endif()
 ENDFUNCTION()
 
+FUNCTION(FINALIZE_SEQUOIA target)
+    include(${CURRENT_DIR}/CompilerOptions.cmake)
+    add_subdirectory(${CURRENT_DIR}/TestFramework TestFramework)
+
+    target_include_directories(${target} PRIVATE ${CURRENT_DIR}/../Source)
+
+    ADD_TEST_INCLUDE_DIRS(${target})
+    target_include_directories(${target} PRIVATE ../TestCommon)
+
+    LINK_LIBRARIES(${target})
+    target_compile_features(${target} PUBLIC cxx_std_20)
+ENDFUNCTION()
+
 FUNCTION(FINALIZE target)
     include(${CURRENT_DIR}/CompilerOptions.cmake)
     add_subdirectory(${CURRENT_DIR}/TestFramework TestFramework)
+    target_include_directories(${target} PRIVATE ${CURRENT_DIR}/../Source/TestFramework)
 
     ADD_TEST_INCLUDE_DIRS(${target})
     LINK_LIBRARIES(${target})
