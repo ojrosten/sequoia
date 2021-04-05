07737998fe9799d2e5d373b6420b5cbe2467038d
Oliver Rosten <oliver.rosten@gmail.com>
2021-04-05 21:39:00 +0100

    Attempt to improve platform independence, plus formatting

--- Tests/Maths/Graph/EdgeTestingDiagnostics.hpp
+++ Tests/Maths/Graph/EdgeTestingDiagnostics.hpp
@@ -7,15 +7,15 @@ namespace sequoia::testing
   class test_edge_false_positives final : public regular_false_positive_test
   {
   public:
-    using regular_false_positive_test::regular_false_positive_test;    
+    using regular_false_positive_test::regular_false_positive_test;
 
     [[nodiscard]]
     std::string_view source_file() const noexcept final;
   private:
     struct null_weight{};
-    
+
     void run_tests() final;
-    
+
     void test_plain_partial_edge();
     void test_partial_edge_indep_weight();
     void test_partial_edge_shared_weight();
@@ -23,8 +23,8 @@ namespace sequoia::testing
     void test_plain_embedded_partial_edge();
     void test_embedded_partial_edge_indep_weight();
     void test_embedded_partial_edge_shared_weight();
-      
-    void test_plain_edge();     
+
+    void test_plain_edge();
     void test_weighted_edge();
 
     void test_plain_embedded_edge();
