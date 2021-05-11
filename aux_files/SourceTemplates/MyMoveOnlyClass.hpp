ba1b37a9a3036f9cdf8be52562c86475b2dd6ec3
Oliver Rosten <oliver.rosten@gmail.com>
2021-05-11 07:46:26 +0100

    Add some code snippets for auto-generation

---
+++ aux_files/SourceTemplates/MyMoveOnlyClass.hpp
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////
+//                Copyright Oliver J. Rosten 2021.                //
+// Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0. //
+//    (See accompanying file LICENSE.md or copy at                //
+//          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
+////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+namespace 
+{
+template<?>
+class ?_class
+{
+public:
+[[nodiscard]]
+friend bool operator==(const ?class&, const ?class&) = default;
+	
+[[nodiscard]]
+friend bool operator!=(const ?class&, const ?class&) = default;
+};
+}
