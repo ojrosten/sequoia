b9069e16767089c506c7ef4d6a9a0c249ee5639f
Oliver Rosten <oliver.rosten@gmail.com>
2021-06-19 17:16:11 +0100

    Start filling in the missing tests

--- Tests/Maths/Graph/DynamicGraphTraversalsTest.cpp
+++ Tests/Maths/Graph/DynamicGraphTraversalsTest.cpp
@@ -497,6 +497,52 @@ namespace sequoia::testing
   template<maths::dynamic_network G, class MessageMaker>
   void test_graph_traversals::test_square_graph(const G& g, const std::size_t start, MessageMaker messageMaker, dfs_type)
   {
+    using traverser_t = Traverser<dfs_type::value>;
+    const auto [nodeDiscovery1, nodeDiscovery2, edgeDiscovery1, edgeDiscovery2] {traverse_graph<traverser_t>(g, maths::ignore_disconnected_t{start})};
+
+    using edge_order = typename decltype(edgeDiscovery1)::result_type;
+    std::vector<std::size_t> nodeAnswers, nodeAnswers2;
+    edge_order edgeAnswers;
+    if constexpr(maths::undirected(G::flavour))
+    {
+      edge_order edgeAnswers2;
+      if(start == 0)
+      {
+        nodeAnswers = std::vector<std::size_t>{0,1,2,3};
+        nodeAnswers2 = std::vector<std::size_t>{3,2,1,0};
+        //edgeAnswers = edge_order{{0, 0}, {0, 1}, {1, 0}, {2, 0}};
+        //edgeAnswers2 = edge_order{{1, 1}, {2, 1}, {3, 0}, {3, 1}};
+      }
+      else if(start == 2)
+      {
+        nodeAnswers = std::vector<std::size_t>{2,1,0,3};
+        nodeAnswers2 = std::vector<std::size_t>{3,0,1,2};
+        //edgeAnswers = edge_order{{2, 0}, {2, 1}, {1, 1}, {0, 0}};
+        //edgeAnswers2 = edge_order{{1, 0}, {0, 1}, {3, 0}, {3, 1}};
+      }
+
+      //check_equivalence(LINE(messageMaker("Second edge traversal, start = " + std::to_string(start) + " ")), edgeDiscovery2, edgeAnswers2);
+    }
+    else
+    {
+      constexpr auto mutualInfo{mutual_info(G::flavour)};
+      if(start == 0)
+      {
+        nodeAnswers = std::vector<std::size_t>{0,1,2,3};
+        nodeAnswers2 = std::vector<std::size_t>{3,2,1,0};
+        //edgeAnswers = mutualInfo ? edge_order{{0, 1}, {1, 0}, {2, 0}, {3, 0}} : edge_order{{0, 0}, {1, 0}, {2, 0}, {3, 0}};
+      }
+      else if(start == 2)
+      {
+        nodeAnswers = std::vector<std::size_t>{2,3,0,1};
+        nodeAnswers2 = std::vector<std::size_t>{1,0,3,2};
+        //edgeAnswers = mutualInfo ? edge_order{{2, 0}, {3, 0}, {0, 1}, {1, 0}} : edge_order{{2, 0}, {3, 0}, {0, 0}, {1, 0}};
+      }
+    }
+
+    check_equivalence(LINE(messageMaker("start = " + std::to_string(start) + " ")), nodeDiscovery1, nodeAnswers);
+    check_equivalence(LINE(messageMaker("start = " + std::to_string(start) + " ")), nodeDiscovery2, nodeAnswers2);
+    //check_equivalence(LINE(messageMaker("First edge traversal, start = " + std::to_string(start) + " ")), edgeDiscovery1, edgeAnswers);
   }
 
   template<maths::dynamic_network G, class MessageMaker>
