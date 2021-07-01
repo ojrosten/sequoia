ec83b5f211073899959027bd1d1e310b741313d5
Oliver Rosten <oliver.rosten@gmail.com>
2021-07-01 21:33:25 +0100

    Next iteration

--- Source/sequoia/TestFramework/DependencyAnalyzer.cpp
+++ Source/sequoia/TestFramework/DependencyAnalyzer.cpp
@@ -7,12 +7,79 @@
 
 #include "sequoia/TestFramework/DependencyAnalyzer.hpp"
 
+#include "sequoia/Streaming/Streaming.hpp"
+
 namespace sequoia::testing
 {
+  namespace fs = std::filesystem;
+
+  namespace
+  {
+    [[nodiscard]]
+    tests_dependency_graph::size_type add_file(tests_dependency_graph& g, const fs::path& file, const fs::path& repo)
+    {
+      if(auto inGraph{std::find_if(g.cbegin_node_weights(), g.cend_node_weights(), [&file](const auto& w) { return w == file; })};
+        inGraph == g.cend_node_weights())
+      {
+        return g.add_node(fs::relative(file, repo));
+      }
+      else
+      {
+        using size_type = tests_dependency_graph::size_type;
+        return static_cast<size_type>(distance(g.cbegin_node_weights(), inGraph));
+      }
+    }
+
+    void build_dependency_graph(tests_dependency_graph& g, const std::filesystem::path& repo)
+    {
+      constexpr std::array<std::string_view, 5> exts{".h", ".hpp", ".cpp", ".cc", ".cxx"};
+
+      for(const auto& entry : fs::recursive_directory_iterator(repo))
+      {
+        const auto file{entry.path()};
+        if(auto found{std::find(exts.begin(), exts.end(), file.extension().string())}; found != exts.end())
+        {
+          const auto nodePos{add_file(g, file, repo)};
+
+          const auto text{read_to_string(entry.path())};
+          std::string::size_type pos{};
+          constexpr auto npos{std::string::npos};
+          while(pos != npos)
+          {
+            constexpr std::string_view include{"#include"};
+            pos = text.find(include, pos);
+            if(pos != npos)
+            {
+              pos += include.size();
+              const auto start{text.find_first_of("\"<", pos)};
+              const auto end{text.find_first_of("\">", start + 1)};
+              if((start != npos) && (end != npos) && (((text[start] == '\"') && (text[end] == '\"')) || ((text[start] == '<') && (text[end] == '>'))))
+              {
+                fs::path includedFile{text.substr(start + 1, end - 1 - start)};
+                if(includedFile.has_extension())
+                {
+                  if(includedFile.parent_path().empty())
+                  {
+                    includedFile = file.parent_path() / includedFile;
+                  }
+
+                  const auto includeNodePos{add_file(g, includedFile, repo)};
+                  g.join(nodePos, includeNodePos);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
   [[nodiscard]]
   tests_dependency_graph build_dependency_graph(const std::filesystem::path& sourceRepo, const std::filesystem::path& testRepo)
   {
     tests_dependency_graph g{};
+    build_dependency_graph(g, sourceRepo);
+    build_dependency_graph(g, testRepo);
 
     return g;
   }
