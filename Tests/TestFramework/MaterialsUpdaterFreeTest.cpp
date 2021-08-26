c61a213b7e046a2b1ac9d9e2994419e1b6b253c2
Oliver Rosten <oliver.rosten@gmail.com>
2021-08-26 22:14:53 +0100

    Test added

--- Tests/TestFramework/MaterialsUpdaterFreeTest.cpp
+++ Tests/TestFramework/MaterialsUpdaterFreeTest.cpp
@@ -29,5 +29,9 @@ namespace sequoia::testing
     check_equality(LINE("Ensure that a target file equivalent to its replacement is not replaced"),
                    read_to_string(working_materials() / "DirToBeKept" / "Comments.txt"),
                    read_to_string(predictive_materials() / "DirToBeKept" / "Comments.txt"));
+
+    check(LINE("Ensure fidelity of previous check"),
+              read_to_string(working_materials() / "DirToBeKept" / "Comments.txt")
+          !=  read_to_string(auxiliary_materials() / "DirToBeKept" / "Comments.txt"));
   }
 }
