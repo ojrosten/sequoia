0473bc0d6c2ffd0964f5029230c40d3a021961e9
Oliver Rosten <oliver.rosten@gmail.com>
2022-06-26 21:08:18 +0100

    Correction

--- Source/sequoia/TestFramework/IndividualTestPaths.cpp
+++ Source/sequoia/TestFramework/IndividualTestPaths.cpp
@@ -5,8 +5,6 @@
 //          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
 ////////////////////////////////////////////////////////////////////
 
-#pragma once
-
 /*! \file
     \brief Definitions for IndividualTestPaths.hpp
  */
