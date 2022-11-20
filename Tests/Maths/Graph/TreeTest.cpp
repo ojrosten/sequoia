ec7f6a97b32f84fb91b864b074a02f7065bb9192
Oliver Rosten <oliver.rosten@gmail.com>
2022-11-20 19:53:50 +0000

    More testing

--- Tests/Maths/Graph/TreeTest.cpp
+++ Tests/Maths/Graph/TreeTest.cpp
@@ -87,7 +87,6 @@ namespace sequoia::testing
       {
         // empty
         tree_type{},
-        //{LINE(""), initCheckFn, initializer{}},
         // 42
         {LINE(""), initCheckFn, initializer{42}},
         // -7
@@ -124,6 +123,73 @@ namespace sequoia::testing
   template<maths::directed_flavour Directedness, maths::tree_link_direction TreeLinkDir>
   void tree_test::test_tree_unweighted_nodes(maths::directed_flavour_constant<Directedness>, maths::tree_link_direction_constant<TreeLinkDir>)
   {
+    using tree_type   = tree<Directedness, TreeLinkDir, null_weight, null_weight>;
+    using initializer = tree_initializer<null_weight>;
+
+    auto initCheckFn{
+      [this](std::string_view message, const tree_type& t, initializer i) {
+        check(equivalence, message, t, i);
+      }
+    };
+
+    using transition_checker_type = transition_checker<tree_type>;
+    using tree_state_graph        = typename transition_checker_type::transition_graph;
+    using edge_t                  = typename transition_checker_type::edge;
+
+    tree_state_graph g{
+      {
+        {
+          edge_t{1, LINE("Add node to empty tree"), [](tree_type t) { t.add_node(tree_type::npos); return t; }}
+        }, // end node 0 edges
+        {
+          edge_t{2, LINE("Add second node"), [](tree_type t) { t.add_node(0); return t; }},
+        }, // end node 1 edges
+        {
+          edge_t{3, LINE("Add third node"),    [](tree_type t) { t.add_node(0); return t; }},
+          edge_t{1, LINE("Prune single node"), [](tree_type t) { t.prune(1); return t; }},
+          edge_t{0, LINE("Prune both nodes"),  [](tree_type t) { t.prune(0); return t; }}
+        }, // end node 2 edges
+        {
+          edge_t{0, LINE("Prune three nodes"),   [](tree_type t) { t.prune(0); return t; }},
+          edge_t{2, LINE("Prune right node"),    [](tree_type t) { t.prune(2); return t; }},
+          edge_t{4, LINE("Add to right branch"), [](tree_type t) { t.add_node(2); return t; }}
+        }, // end node 3 edges
+        {
+          edge_t{0, LINE("Prune four nodes"),   [](tree_type t) { t.prune(0); return t; }},
+          edge_t{2, LINE("Prune right branch"), [](tree_type t) { t.prune(2); return t; }}
+        } // end node 4 edges
+      }, // end edges
+      {
+        // empty
+        tree_type{},
+        // x
+        {LINE(""), initCheckFn, initializer{}},
+        // x
+        //  \
+        //   x
+        {LINE(""), initCheckFn, initializer{{{}}}},
+        // x   x
+        //  \ /
+        //   42
+        {LINE(""), initCheckFn, initializer{{{}, {}}}},
+        //       x
+        //      /
+        // x   x
+        //  \ /
+        //   42
+        {LINE(""), initCheckFn, initializer{{{}, {{{}}}}}}
+      } // end nodes
+    };
+
+    auto checker{
+        [this](std::string_view description, const tree_type& obtained, const tree_type& prediction, const tree_type& parent) {
+          check(equality, description, obtained, prediction);
+          check_semantics(description, prediction, parent);
+        }
+    };
+
+    transition_checker_type::check(LINE(""), g, checker);
+
     using tree_type = tree<Directedness, TreeLinkDir, null_weight, null_weight>;
     using initializer = tree_initializer<null_weight>;
 
