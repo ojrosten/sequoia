5b37397f4c95145560aa14b6727e13e3c4994c99
Oliver Rosten <oliver.rosten@gmail.com>
2023-04-11 08:27:41 +0100

    Experimentation

--- Tests/Experimental/ExperimentalTest.cpp
+++ Tests/Experimental/ExperimentalTest.cpp
@@ -9,8 +9,28 @@
 
 #include "ExperimentalTest.hpp"
 
+#include <iostream>
+#include <source_location>
+
 namespace sequoia::testing
 {
+  namespace
+  {
+    class check_info
+    {
+    public:
+      check_info(std::string_view, std::source_location loc = std::source_location::current())
+        : m_Loc{loc}
+      {
+      }
+
+      [[nodiscard]]
+      const std::source_location& location() const noexcept { return m_Loc; }
+    private:
+      std::source_location m_Loc{};
+    };
+  }
+
   [[nodiscard]]
   std::string_view experimental_test::source_file() const noexcept
   {
@@ -19,5 +39,8 @@ namespace sequoia::testing
 
   void experimental_test::run_tests()
   {
+    check_info c{""};
+    auto loc{c.location()};
+    check(equality, LINE(""), loc.line(), uint_least32_t{42});
   }
 }
