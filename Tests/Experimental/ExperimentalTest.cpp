c0b97e0a14b3014827e04da49c4bfc6b36385481
Oliver Rosten <oliver.rosten@gmail.com>
2023-05-24 19:54:49 +0100

    Proof of concept for constexpr parallel algos

--- Tests/Experimental/ExperimentalTest.cpp
+++ Tests/Experimental/ExperimentalTest.cpp
@@ -8,6 +8,33 @@
 /*! \file */
 
 #include "ExperimentalTest.hpp"
+#include <execution>
+
+namespace experimental
+{
+  template <class _ExecutionPolicy, class _ForwardIterator, class _Predicate>
+  __pstl::__internal::__enable_if_execution_policy<_ExecutionPolicy, bool>
+  constexpr any_of(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIterator __last, _Predicate __pred)
+  {
+    if consteval
+    {
+      return std::any_of(__first, __last, __pred);
+    }
+    else
+    {
+      return __pstl::__internal::__pattern_any_of(
+        std::forward<_ExecutionPolicy>(__exec), __first, __last, __pred,
+        __pstl::__internal::__is_vectorization_preferred<_ExecutionPolicy, _ForwardIterator>(__exec),
+        __pstl::__internal::__is_parallelization_preferred<_ExecutionPolicy, _ForwardIterator>(__exec));
+    }
+  }
+
+  constexpr bool foo()
+  {
+    std::vector<int> v{1,2,3,4,5,6};
+    return experimental::any_of(std::execution::par, v.begin(), v.end(), [](int i){ return i == 4; });
+  }
+}
 
 namespace sequoia::testing
 {
@@ -19,5 +46,7 @@ namespace sequoia::testing
 
   void experimental_test::run_tests()
   {
+    constexpr bool b{experimental::foo()};
+    check(report_line(""), b);
   }
 }
