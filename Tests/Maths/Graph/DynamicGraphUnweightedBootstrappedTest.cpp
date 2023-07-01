9147aace269a7af4c270baa98be8120278c0f94e
Oliver Rosten <oliver.rosten@gmail.com>
2023-07-01 15:19:48 +0100

    More testing

--- Tests/Maths/Graph/DynamicGraphUnweightedBootstrappedTest.cpp
+++ Tests/Maths/Graph/DynamicGraphUnweightedBootstrappedTest.cpp
@@ -1163,6 +1163,38 @@ namespace sequoia::testing
           }
         }, // end 'node_1_node_1_node'
         {  // begin 'node_1_node_1_0_node'
+          {
+            graph_description::node_0_node,
+            "Erase node 0",
+            [](graph_to_test g) -> graph_to_test {
+              g.erase_node(0);
+              return g;
+            }
+          },
+          {
+            graph_description::node_node,
+            "Erase node 1",
+            [](graph_to_test g) -> graph_to_test {
+              g.erase_node(1);
+              return g;
+            }
+          },
+          {
+            graph_description::node_1_node_1_node,
+            "Remove {1,0}",
+            [](graph_to_test g) -> graph_to_test {
+              g.erase_edge(++g.cbegin_edges(1));
+              return g;
+            }
+          },
+          {
+            graph_description::node_1_node_1_0_2_node,
+            "Join {1,2}",
+            [](graph_to_test g) -> graph_to_test {
+              g.join(1, 2);
+              return g;
+            }
+          }
         }, // end 'node_1_node_1_0_node'
         {  // begin 'node_1_node_1_0_2_node'
         }, // end 'node_1_node_1_0_2_node'
