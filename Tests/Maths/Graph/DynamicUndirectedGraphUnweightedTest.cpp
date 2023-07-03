1d622ecae03f26158e6a242754b27756e62e0ae8
Oliver Rosten <oliver.rosten@gmail.com>
2023-07-03 07:16:33 +0100

    More testing

--- Tests/Maths/Graph/DynamicUndirectedGraphUnweightedTest.cpp
+++ Tests/Maths/Graph/DynamicUndirectedGraphUnweightedTest.cpp
@@ -1165,72 +1165,72 @@ namespace sequoia::testing
           }
         }, // end 'node_1_2_node_0_2_node_0_1'
         {  // begin 'node_node_1_node'
-        //  {
-        //    graph_description::empty,
-        //    "Clear graph",
-        //    [](graph_to_test g) -> graph_to_test {
-        //      g.clear();
-        //      return g;
-        //    }
-        //  },
-        //  {
-        //    graph_description::node_0_node,
-        //    "Erase node 0",
-        //    [](graph_to_test g) -> graph_to_test {
-        //      g.erase_node(0);
-        //      return g;
-        //    }
-        //  },
-        //  {
-        //    graph_description::node_node,
-        //    "Erase node 1",
-        //    [](graph_to_test g) -> graph_to_test {
-        //      g.erase_node(1);
-        //      return g;
-        //    }
-        //  },
-        //  {
-        //    graph_description::node_node_1,
-        //    "Erase node 2",
-        //    [](graph_to_test g) -> graph_to_test {
-        //      g.erase_node(2);
-        //      return g;
-        //    }
-        //  },
+          {
+            graph_description::empty,
+            "Clear graph",
+            [](graph_to_test g) -> graph_to_test {
+              g.clear();
+              return g;
+            }
+          },
+          {
+            graph_description::node_0_node,
+            "Erase node 0",
+            [](graph_to_test g) -> graph_to_test {
+              g.erase_node(0);
+              return g;
+            }
+          },
+          {
+            graph_description::node_node,
+            "Erase node 1",
+            [](graph_to_test g) -> graph_to_test {
+              g.erase_node(1);
+              return g;
+            }
+          },
+          {
+            graph_description::node_node_1,
+            "Erase node 2",
+            [](graph_to_test g) -> graph_to_test {
+              g.erase_node(2);
+              return g;
+            }
+          },
         }, // end 'node_node_1_node'
         {  // begin 'node_1_node_0_1_node'
-        //  {
-        //    graph_description::node_0_node,
-        //    "Erase node 0",
-        //    [](graph_to_test g) -> graph_to_test {
-        //      g.erase_node(0);
-        //      return g;
-        //    }
-        //  },
-        //  {
-        //    graph_description::node_node,
-        //    "Erase node 1",
-        //    [](graph_to_test g) -> graph_to_test {
-        //      g.erase_node(1);
-        //      return g;
-        //    }
-        //  },
-        //  {
-        //    graph_description::node_node_1_node,
-        //    "Remove {0,1}",
-        //    [](graph_to_test g) -> graph_to_test {
-        //      g.erase_edge(g.cbegin_edges(0));
-        //      return g;
-        //    }
-        //  },
-        //  {
-        //    graph_description::node_1_node_1_0_node,
-        //    "Join {1,0}",
-        //    [](graph_to_test g) -> graph_to_test {
-        //      g.join(1, 0);
-        //      return g;
-        //    }
-        //  }
+          {
+            graph_description::node_0_node,
+            "Erase node 0",
+            [](graph_to_test g) -> graph_to_test {
+              g.erase_node(0);
+              return g;
+            }
+          },
+          {
+            graph_description::node_node,
+            "Erase node 1",
+            [](graph_to_test g) -> graph_to_test {
+              g.erase_node(1);
+              return g;
+            }
+          },
+          {
+            graph_description::node_node_1_node,
+            "Remove {0,1}",
+            [](graph_to_test g) -> graph_to_test {
+              g.erase_edge(g.cbegin_edges(0));
+              return g;
+            }
+          },
+          {
+            graph_description::node_1_node_0_1_2_node_1,
+            "Join {1,2}",
+            [](graph_to_test g) -> graph_to_test {
+              g.join(1, 2);
+              return g;
+            }
+          }
         }, // end 'node_1_node_0_1_node'
         {  // begin 'node_1_node_0_1_2_node_1'
         //  {
