b70b94e9140c2152879f80b5c4c82e52a7895d0a
Oliver Rosten <oliver.rosten@gmail.com>
2023-07-19 20:01:37 +0100

    More testing

--- Tests/Maths/Graph/DirectedEmbedded/DynamicDirectedEmbeddedGraphWeightedTestingUtilities.hpp
+++ Tests/Maths/Graph/DirectedEmbedded/DynamicDirectedEmbeddedGraphWeightedTestingUtilities.hpp
@@ -347,29 +347,49 @@ namespace sequoia::testing
 
       // end 'graph_description::node_0'
 
-      //// begin 'graph_description::node_0_0'
+      // begin 'graph_description::node_0_0'
 
-      //trg.join(
-      //  graph_description::node_0_0,
-      //  weighted_graph_description::node_0w_0,
-      //  t.report_line("Set zeroth edge weight"),
-      //  [](graph_t g) -> graph_t {
-      //    g.set_edge_weight(g.cbegin_edges(0), 1.0);
-      //    return g;
-      //  }
-      //);
+      trg.join(
+        graph_description::node_0_0,
+        weighted_graph_description::node_0w_0,
+        t.report_line("Set zeroth edge weight via zeroth insertion"),
+        [](graph_t g) -> graph_t {
+          g.set_edge_weight(g.cbegin_edges(0), 1.0);
+          return g;
+        }
+      );
 
-      //trg.join(
-      //  graph_description::node_0_0,
-      //  weighted_graph_description::node_0_0w,
-      //  t.report_line("Set first edge weight"),
-      //  [](graph_t g) -> graph_t {
-      //    g.set_edge_weight(++g.cbegin_edges(0), 1.0);
-      //    return g;
-      //  }
-      //);
+      trg.join(
+        graph_description::node_0_0,
+        weighted_graph_description::node_0w_0,
+        t.report_line("Set zeroth edge weight via first insertion"),
+        [](graph_t g) -> graph_t {
+          g.set_edge_weight(++g.cbegin_edges(0), 1.0);
+          return g;
+        }
+      );
 
-      //// end 'graph_description::node_0_0'
+      trg.join(
+        graph_description::node_0_0,
+        weighted_graph_description::node_0_0w,
+        t.report_line("Set first edge weight via zeroth insertion"),
+        [](graph_t g) -> graph_t {
+          g.set_edge_weight(g.cbegin_edges(0) + 2, 1.0);
+          return g;
+        }
+      );
+
+      trg.join(
+        graph_description::node_0_0,
+        weighted_graph_description::node_0_0w,
+        t.report_line("Set first edge weight via first insertion"),
+        [](graph_t g) -> graph_t {
+          g.set_edge_weight(g.cbegin_edges(0) + 3, 1.0);
+          return g;
+        }
+      );
+
+      // end 'graph_description::node_0_0'
 
       //// begin 'graph_description::node_1_1_node'
 
