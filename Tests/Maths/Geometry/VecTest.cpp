378ae3b29c0d93eef8941c3f8deed6295aec6be6
Oliver Rosten <oliver.rosten@gmail.com>
2024-06-17 21:29:09 +0100

    Tidying

--- Tests/Maths/Geometry/VecTest.cpp
+++ Tests/Maths/Geometry/VecTest.cpp
@@ -38,38 +38,38 @@ namespace sequoia::testing
   template<class T>
   void vec_test::test_vec_1_orderable()
   {
-      using vec_t       = vec<my_vec_space<T, 1>>;
-      using angle_graph = transition_checker<vec_t>::transition_graph;
-      using edge_t      = transition_checker<vec_t>::edge;
+      using vec_t     = vec<my_vec_space<T, 1>>;
+      using vec_graph = transition_checker<vec_t>::transition_graph;
+      using edge_t    = transition_checker<vec_t>::edge;
 
-      angle_graph g{
+      vec_graph g{
         {
           {
-            edge_t{vec_1_label::one,     "- -1",  [](vec_t v) -> vec_t { return -v;  }, std::weak_ordering::greater},
-            edge_t{vec_1_label::neg_one, "+ -1",  [](vec_t v) -> vec_t { return +v;  }, std::weak_ordering::equivalent}
+            edge_t{vec_1_label::one,     "- (-1)",  [](vec_t v) -> vec_t { return -v;  }, std::weak_ordering::greater},
+            edge_t{vec_1_label::neg_one, "+ (-1)",  [](vec_t v) -> vec_t { return +v;  }, std::weak_ordering::equivalent}
           }, // neg_one
           {
-            edge_t{vec_1_label::one, "0 + 1",  [](vec_t v) -> vec_t { return v + vec_t{T(1)};  }, std::weak_ordering::greater},
-            edge_t{vec_1_label::one, "0 += 1", [](vec_t v) -> vec_t { return v += vec_t{T(1)}; }, std::weak_ordering::greater}
+            edge_t{vec_1_label::one, "(0) + (1)",  [](vec_t v) -> vec_t { return v + vec_t{T(1)};  }, std::weak_ordering::greater},
+            edge_t{vec_1_label::one, "(0) += (1)", [](vec_t v) -> vec_t { return v += vec_t{T(1)}; }, std::weak_ordering::greater}
           }, // zero
           {
-            edge_t{vec_1_label::neg_one, "-1",        [](vec_t v) -> vec_t { return -v;               }, std::weak_ordering::less},
-            edge_t{vec_1_label::zero,    "1 - 1",     [](vec_t v) -> vec_t { return v - vec_t{T(1)};  }, std::weak_ordering::less},
-            edge_t{vec_1_label::zero,    "1 -= 1",    [](vec_t v) -> vec_t { return v -= vec_t{T(1)}; }, std::weak_ordering::less},
-            edge_t{vec_1_label::zero,    "1 * T{}",   [](vec_t v) -> vec_t { return v * T{};          }, std::weak_ordering::less},
-            edge_t{vec_1_label::zero,    "T{} * 1",   [](vec_t v) -> vec_t { return T{} *v;           }, std::weak_ordering::less},
-            edge_t{vec_1_label::zero,    "1 *= T(0)", [](vec_t v) -> vec_t { return v *= T{};         }, std::weak_ordering::less},
-            edge_t{vec_1_label::one,     "-1",        [](vec_t v) -> vec_t { return +v;               }, std::weak_ordering::equivalent},
-            edge_t{vec_1_label::two,     "1 + 1",     [](vec_t v) -> vec_t { return v + v;            }, std::weak_ordering::greater},
-            edge_t{vec_1_label::two,     "1 += 1",    [](vec_t v) -> vec_t { return v += v;           }, std::weak_ordering::greater},
-            edge_t{vec_1_label::two,     "1 * T(2)",  [](vec_t v) -> vec_t { return v * T(2);         }, std::weak_ordering::greater},
-            edge_t{vec_1_label::two,     "1 *= T(2)", [](vec_t v) -> vec_t { return v *= T(2);        }, std::weak_ordering::greater},
-            edge_t{vec_1_label::two,     "T(2) * 1",  [](vec_t v) -> vec_t { return T(2) * v;         }, std::weak_ordering::greater}
+            edge_t{vec_1_label::neg_one, "-(1)",        [](vec_t v) -> vec_t { return -v;               }, std::weak_ordering::less},
+            edge_t{vec_1_label::zero,    "(1) - (1)",   [](vec_t v) -> vec_t { return v - vec_t{T(1)};  }, std::weak_ordering::less},
+            edge_t{vec_1_label::zero,    "(1) -= (1)",  [](vec_t v) -> vec_t { return v -= vec_t{T(1)}; }, std::weak_ordering::less},
+            edge_t{vec_1_label::zero,    "(1) * T{}",   [](vec_t v) -> vec_t { return v * T{};          }, std::weak_ordering::less},
+            edge_t{vec_1_label::zero,    "T{} * (1)",   [](vec_t v) -> vec_t { return T{} *v;           }, std::weak_ordering::less},
+            edge_t{vec_1_label::zero,    "(1) *= T(0)", [](vec_t v) -> vec_t { return v *= T{};         }, std::weak_ordering::less},
+            edge_t{vec_1_label::one,     "+(1)",        [](vec_t v) -> vec_t { return +v;               }, std::weak_ordering::equivalent},
+            edge_t{vec_1_label::two,     "(1) + (1)",   [](vec_t v) -> vec_t { return v + v;            }, std::weak_ordering::greater},
+            edge_t{vec_1_label::two,     "(1) += (1)",  [](vec_t v) -> vec_t { return v += v;           }, std::weak_ordering::greater},
+            edge_t{vec_1_label::two,     "(1) * T(2)",  [](vec_t v) -> vec_t { return v * T(2);         }, std::weak_ordering::greater},
+            edge_t{vec_1_label::two,     "(1) *= T(2)", [](vec_t v) -> vec_t { return v *= T(2);        }, std::weak_ordering::greater},
+            edge_t{vec_1_label::two,     "T(2) * (1)",  [](vec_t v) -> vec_t { return T(2) * v;         }, std::weak_ordering::greater}
           }, // one
           {
-            edge_t{vec_1_label::one, "2 / T(2)",  [](vec_t v) -> vec_t { return v / T(2);        }, std::weak_ordering::less},
-            edge_t{vec_1_label::one, "2 /= T(2)", [](vec_t v) -> vec_t { return v /= T(2);       }, std::weak_ordering::less},
-            edge_t{vec_1_label::one, "2 - 1",     [](vec_t v) -> vec_t { return v - vec_t{T(1)}; }, std::weak_ordering::less}
+            edge_t{vec_1_label::one, "(2) / T(2)",  [](vec_t v) -> vec_t { return v / T(2);        }, std::weak_ordering::less},
+            edge_t{vec_1_label::one, "(2) /= T(2)", [](vec_t v) -> vec_t { return v /= T(2);       }, std::weak_ordering::less},
+            edge_t{vec_1_label::one, "(2) - (1)",   [](vec_t v) -> vec_t { return v - vec_t{T(1)}; }, std::weak_ordering::less}
           }, // two
         },
         {vec_t{T(-1)}, vec_t{}, vec_t{T(1)}, vec_t{T(2)}}
@@ -89,37 +89,38 @@ namespace sequoia::testing
   template<class T>
   void vec_test::test_vec_1_unorderable()
   {
-    using vec_t = vec<my_vec_space<T, 1>>;
-    using angle_graph = transition_checker<vec_t>::transition_graph;
-    using edge_t = transition_checker<vec_t>::edge;
+    using vec_t     = vec<my_vec_space<T, 1>>;
+    using vec_graph = transition_checker<vec_t>::transition_graph;
+    using edge_t    = transition_checker<vec_t>::edge;
 
-    angle_graph g{
+    vec_graph g{
       {
         {
-          edge_t{vec_1_label::one,     "- -1",  [](vec_t v) -> vec_t { return -v;  }},
-          edge_t{vec_1_label::neg_one, "+ -1",  [](vec_t v) -> vec_t { return +v;  }}
+          edge_t{vec_1_label::one,     "- (-1)",  [](vec_t v) -> vec_t { return -v;  }},
+          edge_t{vec_1_label::neg_one, "+ (-1)",  [](vec_t v) -> vec_t { return +v;  }}
         }, // neg_one
         {
-          edge_t{vec_1_label::one, "0 + 1",  [](vec_t v) -> vec_t { return v + vec_t{T(1)};  }},
-          edge_t{vec_1_label::one, "0 += 1", [](vec_t v) -> vec_t { return v += vec_t{T(1)}; }}
+          edge_t{vec_1_label::one, "(0) + (1)",  [](vec_t v) -> vec_t { return v + vec_t{T(1)};  }},
+          edge_t{vec_1_label::one, "(0) += (1)", [](vec_t v) -> vec_t { return v += vec_t{T(1)}; }}
         }, // zero
         {
-          edge_t{vec_1_label::neg_one, "-1",        [](vec_t v) -> vec_t { return -v;               }},
-          edge_t{vec_1_label::zero,    "1 - 1",     [](vec_t v) -> vec_t { return v - vec_t{T(1)};  }},
-          edge_t{vec_1_label::zero,    "1 -= 1",    [](vec_t v) -> vec_t { return v -= vec_t{T(1)}; }},
-          edge_t{vec_1_label::zero,    "1 * T{}",   [](vec_t v) -> vec_t { return v * T{};          }},
-          edge_t{vec_1_label::zero,    "T{} * 1",   [](vec_t v) -> vec_t { return T{} *v;           }},
-          edge_t{vec_1_label::zero,    "1 *= T(0)", [](vec_t v) -> vec_t { return v *= T{};         }},
-          edge_t{vec_1_label::two,     "1 + 1",     [](vec_t v) -> vec_t { return v + v;            }},
-          edge_t{vec_1_label::two,     "1 += 1",    [](vec_t v) -> vec_t { return v += v;           }},
-          edge_t{vec_1_label::two,     "1 * T(2)",  [](vec_t v) -> vec_t { return v * T(2);         }},
-          edge_t{vec_1_label::two,     "1 *= T(2)", [](vec_t v) -> vec_t { return v *= T(2);        }},
-          edge_t{vec_1_label::two,     "T(2) * 1",  [](vec_t v) -> vec_t { return T(2) * v;         }}
+          edge_t{vec_1_label::neg_one, "-(1)",        [](vec_t v) -> vec_t { return -v;               }},
+          edge_t{vec_1_label::zero,    "(1) - (1)",   [](vec_t v) -> vec_t { return v - vec_t{T(1)};  }},
+          edge_t{vec_1_label::zero,    "(1) -= (1)",  [](vec_t v) -> vec_t { return v -= vec_t{T(1)}; }},
+          edge_t{vec_1_label::zero,    "(1) * T{}",   [](vec_t v) -> vec_t { return v * T{};          }},
+          edge_t{vec_1_label::zero,    "T{} * (1)",   [](vec_t v) -> vec_t { return T{} *v;           }},
+          edge_t{vec_1_label::zero,    "(1) *= T(0)", [](vec_t v) -> vec_t { return v *= T{};         }},
+          edge_t{vec_1_label::one,     "+(1)",        [](vec_t v) -> vec_t { return +v;               }},
+          edge_t{vec_1_label::two,     "(1) + (1)",   [](vec_t v) -> vec_t { return v + v;            }},
+          edge_t{vec_1_label::two,     "(1) += (1)",  [](vec_t v) -> vec_t { return v += v;           }},
+          edge_t{vec_1_label::two,     "(1) * T(2)",  [](vec_t v) -> vec_t { return v * T(2);         }},
+          edge_t{vec_1_label::two,     "(1) *= T(2)", [](vec_t v) -> vec_t { return v *= T(2);        }},
+          edge_t{vec_1_label::two,     "T(2) * (1)",  [](vec_t v) -> vec_t { return T(2) * v;         }}
         }, // one
         {
-          edge_t{vec_1_label::one, "2 / T(2)",  [](vec_t v) -> vec_t { return v / T(2);        }},
-          edge_t{vec_1_label::one, "2 /= T(2)", [](vec_t v) -> vec_t { return v /= T(2);       }},
-          edge_t{vec_1_label::one, "2 - 1",     [](vec_t v) -> vec_t { return v - vec_t{T(1)}; }}
+          edge_t{vec_1_label::one, "(2) / T(2)",  [](vec_t v) -> vec_t { return v / T(2);        }},
+          edge_t{vec_1_label::one, "(2) /= T(2)", [](vec_t v) -> vec_t { return v /= T(2);       }},
+          edge_t{vec_1_label::one, "(2) - (1)",   [](vec_t v) -> vec_t { return v - vec_t{T(1)}; }}
         }, // two
       },
       {vec_t{T(-1)}, vec_t{}, vec_t{T(1)}, vec_t{T(2)}}
