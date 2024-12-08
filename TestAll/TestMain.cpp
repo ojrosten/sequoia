96fe7df014a5b9f7f43d1ebee13f9f32bb3e1ca0
Oliver Rosten <oliver.rosten@gmail.com>
2024-12-08 16:58:20 +0000

    Add to the TestAll project

--- TestAll/TestMain.cpp
+++ TestAll/TestMain.cpp
@@ -389,6 +389,12 @@ int main(int argc, char** argv)
       }
     );
 
+    runner.add_test_suite(
+      "Quantity",
+      quantity_false_negative_test{"False Negative Test"},
+      quantity_test{"Unit Test"}
+    );
+
     runner.execute(timer_resolution{1ms});
   }
   catch(const std::exception& e)
