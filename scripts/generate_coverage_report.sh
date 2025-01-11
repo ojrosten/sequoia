f7863895b3ca20a1ca5a8ac0116a95e9cb754014
ojrosten <oliver.rosten@gmail.com>
2025-01-11 16:16:42 +0000

    Add some basic code coverage functionality and reporting

---
+++ scripts/generate_coverage_report.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check if a test directory was provided as an argument
+if [[ -z "$1" ]]; then
+  echo "Usage: $0 <Test Executable Directory>"
+  exit 1
+fi
+
+# Get the directory of the script
+script_dir=$(dirname "$0")
+
+# Get the test directory from the first argument
+test_dir="$1"
+
+# Cleanup lcov
+lcov --zerocounters --directory "$test_Dir"
+
+# Run cTest to ensure RenderingSetup.txt is generatated
+cd "$test_dir"
+ctest -T Test
+
+# Relative location of the html output directory
+output_dir="${script_dir}/../coverage_reports/$(basename "$PWD")"
+
+# Create output directory if it doesn't exist
+mkdir -p "$output_dir"
+
+# Run ctest in coverage mode
+ctest -T Coverage
+
+# Generate lcov coverage report
+lcov --directory "$test_dir".  --capture --output-file coverage.info --keep-going --rc geninfo_unexecuted_blocks=1
+lcov --remove coverage.info '/usr/*' --output-file coverage.info
+
+# Generate HTML report
+genhtml --demangle-cpp -o "${output_dir}" coverage.info
