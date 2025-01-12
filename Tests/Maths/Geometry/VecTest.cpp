5b4c2d6805daa07a81ce3fef18a2aac3d95090a8
Oliver Rosten <oliver.rosten@gmail.com>
2025-01-12 16:28:59 +0000

    Experimentation

--- Tests/Maths/Geometry/VecTest.cpp
+++ Tests/Maths/Geometry/VecTest.cpp
@@ -17,6 +17,28 @@ namespace sequoia::testing
 {
   using namespace maths;
 
+  template<class T, class U>
+  struct direct_product
+  {
+    using set_type = std::pair<T, U>;
+  };
+
+  template<vector_space T, vector_space U>
+  struct direct_product<T, U>
+  {
+    using set_type   = direct_product<typename T::set_type, typename U::set_type>;
+    using field_type = std::common_type_t<typename T::field_type, typename U::field_type>;
+    constexpr static std::size_t dimension{T::dimension * U::dimension};
+  };
+  
+  template<convex_space T, convex_space U>
+    requires (!vector_space<T> && !vector_space<U>)
+  struct direct_product<T, U>
+  {
+    using set_type          = direct_product<typename T::set_type, typename U::set_type>;
+    using vector_space_type = direct_product<typename T::vector_space_type, typename U::vector_space_type>;
+  };
+
   // TO DO: move these
   static_assert(has_plus_v<int>);
   static_assert(has_minus_v<int>);
@@ -28,6 +50,7 @@ namespace sequoia::testing
   static_assert(!weakly_abelian_group_under_multiplication_v<int>);
   static_assert(weakly_abelian_group_under_multiplication_v<double>);
 
+  // TO DO: move this to physics examples
   namespace
   {
     template<std::size_t D, std::floating_point T>
@@ -91,6 +114,8 @@ namespace sequoia::testing
   {
     using vec_t = vector_coordinates<my_vec_space<Set, Field, D>, canonical_basis<Set, Field, D>>;
     coordinates_operations<vec_t>{*this}.execute();
+
+    static_assert(vector_space<direct_product<vec_t, vec_t>>);
   }
 
   template<class Set, std::floating_point Field>
