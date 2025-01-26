3fcaf622c0e0bb98162cd96d2da76e1dea44bff6
Oliver Rosten <oliver.rosten@gmail.com>
2025-01-26 19:17:10 +0000

    Bit the bullet and introduce a macro for writing static asserts that are counted as runtime checks

--- Source/sequoia/TestFramework/FreeCheckers.hpp
+++ Source/sequoia/TestFramework/FreeCheckers.hpp
@@ -772,6 +772,8 @@ namespace sequoia::testing
     {
       return testing::check_exception_thrown<E>(self.report(description), self.m_Logger, std::forward<Fn>(function), std::move(postprocessor));
     }
+    
+#define STATIC_CHECK(Boolean, Description) (check("", [](){ static_assert(Boolean, Description); return true; }()))
 
     template<class Stream>
       requires serializable_to<Stream, test_logger<Mode>>
--- Tests/Core/Meta/TypeAlgorithmsFreeTest.cpp
+++ Tests/Core/Meta/TypeAlgorithmsFreeTest.cpp
@@ -38,69 +38,69 @@ namespace sequoia::testing
 
   void type_algorithms_free_test::test_lower_bound()
   {
-    static_assert(lower_bound_v<std::tuple<>,     int, comparator> == 0);
-    static_assert(lower_bound_v<std::tuple<int>,  int, comparator> == 0);
-    static_assert(lower_bound_v<std::tuple<char>, int, comparator> == 1);
-    static_assert(lower_bound_v<std::tuple<char, short, int>, char,   comparator> == 0);
-    static_assert(lower_bound_v<std::tuple<char, short, int>, short,  comparator> == 1);
-    static_assert(lower_bound_v<std::tuple<char, short, int>, int,    comparator> == 2);
-    static_assert(lower_bound_v<std::tuple<char, short, int>, double, comparator> == 3);
-    static_assert(lower_bound_v<std::tuple<char, char, short, short, int, int>, char,  comparator> == 0);
-    static_assert(lower_bound_v<std::tuple<char, char, short, short, int, int>, short, comparator> == 2);
-    static_assert(lower_bound_v<std::tuple<char, char, short, short, int, int>, int,   comparator> == 4);
+    STATIC_CHECK((lower_bound_v<std::tuple<>,     int, comparator> == 0), "");
+    STATIC_CHECK((lower_bound_v<std::tuple<int>,  int, comparator> == 0), "");
+    STATIC_CHECK((lower_bound_v<std::tuple<char>, int, comparator> == 1), "");
+    STATIC_CHECK((lower_bound_v<std::tuple<char, short, int>, char,   comparator> == 0), "");
+    STATIC_CHECK((lower_bound_v<std::tuple<char, short, int>, short,  comparator> == 1), "");
+    STATIC_CHECK((lower_bound_v<std::tuple<char, short, int>, int,    comparator> == 2), "");
+    STATIC_CHECK((lower_bound_v<std::tuple<char, short, int>, double, comparator> == 3), "");
+    STATIC_CHECK((lower_bound_v<std::tuple<char, char, short, short, int, int>, char,  comparator> == 0), "");
+    STATIC_CHECK((lower_bound_v<std::tuple<char, char, short, short, int, int>, short, comparator> == 2), "");
+    STATIC_CHECK((lower_bound_v<std::tuple<char, char, short, short, int, int>, int,   comparator> == 4), "");
   }
 
   void type_algorithms_free_test::test_filter()
   {   
-    static_assert(std::is_same_v<filter_t<std::tuple<>, std::index_sequence<>>, std::tuple<>>);
-    static_assert(std::is_same_v<filter_t<std::tuple<int>, std::index_sequence<>>, std::tuple<>>);
-    static_assert(std::is_same_v<filter_t<std::tuple<int>, std::index_sequence<0>>, std::tuple<int>>);
-    static_assert(std::is_same_v<filter_t<std::tuple<int, float>, std::index_sequence<0>>, std::tuple<int>>);
-    static_assert(std::is_same_v<filter_t<std::tuple<int, float>, std::index_sequence<1>>, std::tuple<float>>);
-    static_assert(std::is_same_v<filter_t<std::tuple<int, float>, std::index_sequence<0, 1>>, std::tuple<int, float>>);
+    STATIC_CHECK((std::is_same_v<filter_t<std::tuple<>, std::index_sequence<>>, std::tuple<>>), "");
+    STATIC_CHECK((std::is_same_v<filter_t<std::tuple<int>, std::index_sequence<>>, std::tuple<>>), "");
+    STATIC_CHECK((std::is_same_v<filter_t<std::tuple<int>, std::index_sequence<0>>, std::tuple<int>>), "");
+    STATIC_CHECK((std::is_same_v<filter_t<std::tuple<int, float>, std::index_sequence<0>>, std::tuple<int>>), "");
+    STATIC_CHECK((std::is_same_v<filter_t<std::tuple<int, float>, std::index_sequence<1>>, std::tuple<float>>), "");
+    STATIC_CHECK((std::is_same_v<filter_t<std::tuple<int, float>, std::index_sequence<0, 1>>, std::tuple<int, float>>), "");
   }
 
   void type_algorithms_free_test::test_drop()
   {
-    static_assert(std::is_same_v<drop_t<std::tuple<>, 0>, std::tuple<>>);
-    static_assert(std::is_same_v<drop_t<std::tuple<int>, 0>, std::tuple<int>>);
-    static_assert(std::is_same_v<drop_t<std::tuple<int>, 1>, std::tuple<>>);
-    static_assert(std::is_same_v<drop_t<std::tuple<char, int>, 1>, std::tuple<int>>);
-    static_assert(std::is_same_v<drop_t<std::tuple<char, int>, 2>, std::tuple<>>);
+    STATIC_CHECK((std::is_same_v<drop_t<std::tuple<>, 0>, std::tuple<>>), "");
+    STATIC_CHECK((std::is_same_v<drop_t<std::tuple<int>, 0>, std::tuple<int>>), "");
+    STATIC_CHECK((std::is_same_v<drop_t<std::tuple<int>, 1>, std::tuple<>>), "");
+    STATIC_CHECK((std::is_same_v<drop_t<std::tuple<char, int>, 1>, std::tuple<int>>), "");
+    STATIC_CHECK((std::is_same_v<drop_t<std::tuple<char, int>, 2>, std::tuple<>>), "");
   }
 
   void type_algorithms_free_test::test_keep()
   {
-    static_assert(std::is_same_v<keep_t<std::tuple<>, 0>, std::tuple<>>);
-    static_assert(std::is_same_v<keep_t<std::tuple<int>, 0>, std::tuple<>>);
-    static_assert(std::is_same_v<keep_t<std::tuple<int>, 1>, std::tuple<int>>);
-    static_assert(std::is_same_v<keep_t<std::tuple<char, int>, 0>, std::tuple<>>);
-    static_assert(std::is_same_v<keep_t<std::tuple<char, int>, 1>, std::tuple<char>>);
-    static_assert(std::is_same_v<keep_t<std::tuple<char, int>, 2>, std::tuple<char, int>>);
+    STATIC_CHECK((std::is_same_v<keep_t<std::tuple<>, 0>, std::tuple<>>), "");
+    STATIC_CHECK((std::is_same_v<keep_t<std::tuple<int>, 0>, std::tuple<>>), "");
+    STATIC_CHECK((std::is_same_v<keep_t<std::tuple<int>, 1>, std::tuple<int>>), "");
+    STATIC_CHECK((std::is_same_v<keep_t<std::tuple<char, int>, 0>, std::tuple<>>), "");
+    STATIC_CHECK((std::is_same_v<keep_t<std::tuple<char, int>, 1>, std::tuple<char>>), "");
+    STATIC_CHECK((std::is_same_v<keep_t<std::tuple<char, int>, 2>, std::tuple<char, int>>), "");
   }
 
   void type_algorithms_free_test::test_merge()
   {
-    static_assert(std::is_same_v<merge_t<std::tuple<>, std::tuple<>, comparator>, std::tuple<>>);
-    static_assert(std::is_same_v<merge_t<std::tuple<>, std::tuple<int>, comparator>, std::tuple<int>>);
-    static_assert(std::is_same_v<merge_t<std::tuple<int>, std::tuple<>, comparator>, std::tuple<int>>);
-    static_assert(std::is_same_v<merge_t<std::tuple<int>, std::tuple<int>,  comparator>, std::tuple<int, int>>);
-    static_assert(std::is_same_v<merge_t<std::tuple<char>, std::tuple<int>, comparator>, std::tuple<char, int>>);
-    static_assert(std::is_same_v<merge_t<std::tuple<int>, std::tuple<char>, comparator>, std::tuple<char, int>>);
-    static_assert(std::is_same_v<merge_t<std::tuple<char>, std::tuple<char, int>, comparator>, std::tuple<char, char, int>>);
-    static_assert(std::is_same_v<merge_t<std::tuple<short>, std::tuple<char, int>, comparator>, std::tuple<char, short, int>>);
-    static_assert(std::is_same_v<merge_t<std::tuple<double>, std::tuple<char, int>, comparator>, std::tuple<char, int, double>>);
-    static_assert(std::is_same_v<merge_t<std::tuple<char, int>, std::tuple<char>, comparator>, std::tuple<char, char, int>>);
-    static_assert(std::is_same_v<merge_t<std::tuple<char, int>, std::tuple<char, short>, comparator>, std::tuple<char, char, short, int>>);
-    static_assert(std::is_same_v<merge_t<std::tuple<short, int>, std::tuple<char, short, double>, comparator>, std::tuple<char, short, short, int, double>>);
+    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<>, std::tuple<>, comparator>, std::tuple<>>), "");
+    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<>, std::tuple<int>, comparator>, std::tuple<int>>), "");
+    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<int>, std::tuple<>, comparator>, std::tuple<int>>), "");
+    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<int>, std::tuple<int>,  comparator>, std::tuple<int, int>>), "");
+    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<char>, std::tuple<int>, comparator>, std::tuple<char, int>>), "");
+    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<int>, std::tuple<char>, comparator>, std::tuple<char, int>>), "");
+    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<char>, std::tuple<char, int>, comparator>, std::tuple<char, char, int>>), "");
+    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<short>, std::tuple<char, int>, comparator>, std::tuple<char, short, int>>), "");
+    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<double>, std::tuple<char, int>, comparator>, std::tuple<char, int, double>>), "");
+    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<char, int>, std::tuple<char>, comparator>, std::tuple<char, char, int>>), "");
+    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<char, int>, std::tuple<char, short>, comparator>, std::tuple<char, char, short, int>>), "");
+    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<short, int>, std::tuple<char, short, double>, comparator>, std::tuple<char, short, short, int, double>>), "");
   }
 
   void type_algorithms_free_test::test_stable_sort()
   {
-    static_assert(std::is_same_v<stable_sort_t<std::tuple<>, comparator>, std::tuple<>>);
-    static_assert(std::is_same_v<stable_sort_t<std::tuple<int>, comparator>, std::tuple<int>>);
-    static_assert(std::is_same_v<stable_sort_t<std::tuple<char, int>, comparator>, std::tuple<char, int>>);
-    static_assert(std::is_same_v<stable_sort_t<std::tuple<int, char>, comparator>, std::tuple<char, int>>);
-    static_assert(std::is_same_v<stable_sort_t<std::tuple<int, char, double, short>, comparator>, std::tuple<char, short, int, double>>);
+    STATIC_CHECK((std::is_same_v<stable_sort_t<std::tuple<>, comparator>, std::tuple<>>), "");
+    STATIC_CHECK((std::is_same_v<stable_sort_t<std::tuple<int>, comparator>, std::tuple<int>>), "");
+    STATIC_CHECK((std::is_same_v<stable_sort_t<std::tuple<char, int>, comparator>, std::tuple<char, int>>), "");
+    STATIC_CHECK((std::is_same_v<stable_sort_t<std::tuple<int, char>, comparator>, std::tuple<char, int>>), "");
+    STATIC_CHECK((std::is_same_v<stable_sort_t<std::tuple<int, char, double, short>, comparator>, std::tuple<char, short, int, double>>), "");
   }
 }
