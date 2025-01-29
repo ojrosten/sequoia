1db0a6b2b02f005956f9f9169bbea16df76eab8e
Oliver Rosten <oliver.rosten@gmail.com>
2025-01-29 08:13:52 +0000

    More testing

--- Tests/Physics/QuantityTest.cpp
+++ Tests/Physics/QuantityTest.cpp
@@ -71,6 +71,13 @@ namespace sequoia::testing
       static_assert(vector_space<reduction_t<direct_product<displacement_space<classical_quantity_sets::masses, float>, displacement_space<classical_quantity_sets::lengths, float>>>>);
       
       static_assert(convex_space<reduction_t<direct_product<mass_space<float>, length_space<float>>>>);
+      static_assert(std::is_same_v<reduction_t<direct_product<mass_space<float>, length_space<float>>>,
+                                   reduction<ordered_direct_product<std::tuple<length_space<float>, mass_space<float>>>>>);
+      static_assert(std::is_same_v<reduction_t<direct_product<mass_space<float>, length_space<float>>>,
+                                   reduction_t<direct_product<length_space<float>, mass_space<float>>>>);
+
+
+      
       static_assert(std::is_same_v<reduction_t<std::tuple<units::metre_t, units::kilogram_t>>,
                                    reduction_t<std::tuple<units::kilogram_t, units::metre_t>>>);
 
