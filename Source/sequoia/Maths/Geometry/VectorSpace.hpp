81ccf9c8de1d9c9f3c8f200ce02c8067e9a05774
Oliver Rosten <oliver.rosten@gmail.com>
2025-01-29 21:40:21 +0000

    Next iteration

--- Source/sequoia/Maths/Geometry/VectorSpace.hpp
+++ Source/sequoia/Maths/Geometry/VectorSpace.hpp
@@ -364,12 +364,21 @@ namespace sequoia::maths
     using type = reduction<direct_product<meta::merge_t<std::tuple<T, U>, std::tuple<V>, meta::type_comparator>>>;
   };
 
+  template<vector_space T, vector_space U, vector_space V, vector_space W>
+    requires ((T::dimension == 1) && (U::dimension == 1)) || ((V::dimension == 1) && (W::dimension == 1))
+  struct reduction<direct_product<reduction<direct_product<std::tuple<T, U>>>, reduction<direct_product<std::tuple<V, W>>>>>
+  {
+    using type = reduction<direct_product<meta::merge_t<std::tuple<T, U>, std::tuple<V, W>, meta::type_comparator>>>;
+  };
+
   template<convex_space T, convex_space U>
     requires (!vector_space<T> && !vector_space<U>)
   struct reduction<direct_product<T, U>>
   {
     using type = reduction<direct_product<meta::merge_t<std::tuple<T>, std::tuple<U>, meta::type_comparator>>>;
   };
+
+  // TO DO: fill in the gaps for convex spaces
 
   //============================== coordinates_base definition  ==============================//
 
--- Tests/Physics/QuantityTest.cpp
+++ Tests/Physics/QuantityTest.cpp
@@ -69,6 +69,7 @@ namespace sequoia::testing
       using delta_mass_space_t = displacement_space<classical_quantity_sets::masses, float>;
       using delta_len_space_t  = displacement_space<classical_quantity_sets::lengths, float>;
       using delta_temp_space_t = displacement_space<classical_quantity_sets::temperatures, float>;
+      using delta_time_space_t = displacement_space<classical_quantity_sets::times, float>;
    
       static_assert(vector_space<delta_mass_space_t>);
       static_assert(vector_space<delta_len_space_t>);
@@ -80,6 +81,9 @@ namespace sequoia::testing
                                    reduction<direct_product<std::tuple<delta_len_space_t, delta_mass_space_t, delta_temp_space_t>>>>);
       static_assert(std::is_same_v<reduction_t<direct_product<reduction_t<direct_product<delta_mass_space_t, delta_len_space_t>>, delta_temp_space_t>>,
                                    reduction<direct_product<std::tuple<delta_len_space_t, delta_mass_space_t, delta_temp_space_t>>>>);
+      static_assert(std::is_same_v<reduction_t<direct_product<reduction_t<direct_product<delta_mass_space_t, delta_len_space_t>>,
+                                                              reduction_t<direct_product<delta_time_space_t, delta_temp_space_t>>>>,
+                                   reduction<direct_product<std::tuple<delta_len_space_t, delta_mass_space_t, delta_temp_space_t, delta_time_space_t>>>>);
       
       static_assert(convex_space<length_space_t>);
       static_assert(convex_space<mass_space_t>);
