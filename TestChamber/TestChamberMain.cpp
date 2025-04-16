b795dcac505b90eb8ad7ce1cf80d305564fd4ce2
Oliver Rosten <oliver.rosten@gmail.com>
2025-04-16 08:42:40 +0100

    Restore empty test chamber

--- TestChamber/TestChamberMain.cpp
+++ TestChamber/TestChamberMain.cpp
@@ -24,36 +24,6 @@ int main(int argc, char** argv)
                        "  ",
                        {.main_cpp{"TestChamber/TestChamberMain.cpp"}, .common_includes{"TestCommon/TestIncludes.hpp"}}};
 
-    runner.add_test_suite(
-      "Geometry",
-      suite{
-        "Spaces",        
-        spaces_meta_free_test{"Spaces Meta Free Test"}
-      },
-      suite{
-        "Vector Coordinates",
-        vector_coordinates_false_negative_test{"False negative Test"},
-        vector_coordinates_test{"Unit Test"}
-      },
-      suite{
-        "Affine Coordinates",
-        affine_coordinates_false_negative_test{"False negative Test"},
-        affine_coordinates_test{"Unit Test"}
-      }
-    );
-
-    runner.add_test_suite(
-      "Physical Values",
-      physical_value_meta_free_test{"Physical Value Meta Free Test"},
-      physical_value_false_negative_test{"False Negative Test"},
-      absolute_physical_value_test{"Absolute Physical Value Test"},
-      unsafe_absolute_physical_value_test{"Unsafe Absolute Physical Value Test"},
-      affine_physical_value_test{"Affine Physical Value Test"},
-      convex_physical_value_test{"Convex Physical Value Test"},
-      vector_physical_value_test{"Vector Physical Value Test"},
-      mixed_physical_value_test{"Mixed Physical Value Test"}
-    );
-
     runner.execute(timer_resolution{1ms});
   }
   catch(const std::exception& e)
