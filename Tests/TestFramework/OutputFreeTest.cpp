18fced83e520a21f9ba187eb4ff076e8d178ee91
Oliver Rosten <oliver.rosten@gmail.com>
2025-05-16 07:45:02 +0100

    Testing

--- Tests/TestFramework/OutputFreeTest.cpp
+++ Tests/TestFramework/OutputFreeTest.cpp
@@ -47,6 +47,10 @@ namespace sequoia::testing
   void output_free_test::test_tidy_name()
   {
     check(equality, "", tidy_name("(some enum)0", clang_type{}), "0"s);
+    check(equality, "", tidy_name("<0f>", clang_type{}), "<0>"s);
+    check(equality, "", tidy_name("<0ul>", clang_type{}), "<0>"s);
+    check(equality, "", tidy_name("<1ul, 0f>", clang_type{}), "<1, 0>"s);
+    check(equality, "", tidy_name("<2ul, 1f>", clang_type{}), "<2, 1>"s);
     check(equality, "", tidy_name("<textued_2d>", clang_type{}), "<textued_2d>"s);
     check(equality, "", tidy_name("struct foo", msvc_type{}), "foo"s);
   }
