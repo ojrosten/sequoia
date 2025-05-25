a2fe795ae5af94f9bf88c24f0046f88eaf99448e
Oliver Rosten <oliver.rosten@gmail.com>
2025-05-25 22:03:07 +0100

    Extension and tests

--- Tests/TestFramework/OrderableRegularTestDiagnostics.cpp
+++ Tests/TestFramework/OrderableRegularTestDiagnostics.cpp
@@ -77,6 +77,16 @@ namespace sequoia::testing
       check_semantics("Incorrect x value; mutator", beast{1}, beast{2}, equiv_t{2}, equiv_t{2}, std::weak_ordering::less, [](auto& b) { b.x.front() = 3; });
       check_semantics("Incorrect y value; mutator", beast{1}, beast{2}, equiv_t{1}, equiv_t{3}, std::weak_ordering::less, [](auto& b) { b.x.front() = 3; });
     }
+
+    {
+      using beast   = orderable_specified_moved_from_beast<int>;
+      using equiv_t = std::vector<int>;
+      check_semantics("Incorrect moved-from state post construction", beast{1}, beast{2}, equiv_t{1}, equiv_t{2}, equiv_t{1}, equiv_t{}, std::weak_ordering::less);
+      check_semantics("Incorrect moved-from state post construction", beast{1}, beast{2}, equiv_t{1}, equiv_t{2},   beast{1},   beast{}, std::weak_ordering::less);
+
+      check_semantics("Incorrect moved-from state post assignment", beast{1}, beast{2}, equiv_t{1}, equiv_t{2}, equiv_t{}, equiv_t{2}, std::weak_ordering::less);
+      check_semantics("Incorrect moved-from state post assignment", beast{1}, beast{2}, equiv_t{1}, equiv_t{2},   beast{},   beast{2}, std::weak_ordering::less);
+    }
   }
 
   [[nodiscard]]
