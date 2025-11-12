247cce8e35ce8248142756f52c138fa1addb66f8
ojrosten <oliver.rosten@gmail.com>
2025-11-12 20:50:42 +0000

    More script improvements

--- scripts/generate_coverage_report.sh
+++ scripts/generate_coverage_report.sh
@@ -8,28 +8,34 @@ fi
 
 # Get the directory of the script
 script_dir=$(dirname "$0")
+echo "Script Dir: ${script_dir}"
 
-# Get the test directory from the first argument
-test_dir="$1"
+# Get the test executable directory from the first argument
+test_exe_dir_relative="$1"
+test_exe_dir=$(cd "$test_exe_dir_relative" && pwd -P)
+echo "Test Dir: ${test_exe_dir}"
 
 # Cleanup lcov
-lcov --zerocounters --directory "$test_Dir"
+lcov --zerocounters --directory "${test_exe_dir}"
 
 # Get the path components after 'build/'
-path_suffix="${PWD#*build/}"
+path_suffix="${test_exe_dir#*build/}"
 
 # Relative location of the html output directory
 output_dir="${script_dir}/../coverage_reports/${path_suffix}"
+echo "Output Dir: ${output_dir}"
 
 # Create output directory if it doesn't exist
-mkdir -p "$output_dir"
+mkdir -p "${output_dir}"
 
 # Run ctest in coverage mode
+pushd "${test_exe_dir}"
 ctest -T Coverage
+popd
 
 # Generate lcov coverage report
-lcov --directory "$test_dir".  --capture --output-file coverage.info --keep-going --filter range --rc geninfo_unexecuted_blocks=1 --gcov-tool /usr/bin/gcov-15
-lcov --remove coverage.info '/usr/*' --output-file coverage.info --ignore-errors inconsistent
+lcov --directory "${test_exe_dir}"  --capture --output-file "${test_exe_dir}/coverage.info" --keep-going --filter range --rc geninfo_unexecuted_blocks=1 --ignore-errors empty --gcov-tool /usr/bin/gcov-15
+lcov --remove  "${test_exe_dir}/coverage.info" '/usr/*' --output-file "${test_exe_dir}/coverage.info" --ignore-errors inconsistent
 
 # Generate HTML report
-genhtml --demangle-cpp -o "${output_dir}" coverage.info --ignore-errors inconsistent --ignore-errors range
+genhtml --demangle-cpp -o "${output_dir}" "${test_exe_dir}/coverage.info" --ignore-errors inconsistent --ignore-errors range --ignore-errors empty
