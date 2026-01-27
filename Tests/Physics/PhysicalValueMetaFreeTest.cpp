8de29b7ff3b47df090f2788998a74e0461eec3ee
Oliver Rosten <oliver.rosten@gmail.com>
2026-01-27 21:32:18 +0000

    More testing

--- Tests/Physics/PhysicalValueMetaFreeTest.cpp
+++ Tests/Physics/PhysicalValueMetaFreeTest.cpp
@@ -205,7 +205,12 @@ namespace sequoia::testing
 
     STATIC_CHECK(std::is_same_v<reduce_t<count_and_combine_t<direct_product<euc_half_space_t, dual<euc_vec_space_t>>>>,
                                 direct_product<euc_vec_space_t>>);
-    // Test more duals
+
+    STATIC_CHECK(std::is_same_v<reduce_t<count_and_combine_t<direct_product<dual<euc_half_space_t>, euc_vec_space_t>>>,
+                                direct_product<euc_vec_space_t>>);
+
+    STATIC_CHECK(std::is_same_v<reduce_t<count_and_combine_t<direct_product<dual<euc_half_space_t>, dual<euc_vec_space_t>>>>,
+                                direct_product<euc_vec_space_t>>);
   }
 
   void physical_value_meta_free_test::test_simplify()
