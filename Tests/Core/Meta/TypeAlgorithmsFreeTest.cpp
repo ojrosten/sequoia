f6af68877f1c59ef8698e518c03d43c09c6de765
Oliver Rosten <oliver.rosten@gmail.com>
2026-02-04 21:23:06 +0000

    Correct a rather silly test mistake, plus formatting

--- Tests/Core/Meta/TypeAlgorithmsFreeTest.cpp
+++ Tests/Core/Meta/TypeAlgorithmsFreeTest.cpp
@@ -86,114 +86,114 @@ namespace sequoia::testing
   template<template<class...> class TT>
   void type_algorithms_free_test::test_lower_bound()
   {
-    STATIC_CHECK((lower_bound_v<TT<>,     int, comparator> == 0));
-    STATIC_CHECK((lower_bound_v<TT<int>,  int, comparator> == 0));
-    STATIC_CHECK((lower_bound_v<TT<char>, int, comparator> == 1));
-    STATIC_CHECK((lower_bound_v<TT<char, short, int>, char,   comparator> == 0));
-    STATIC_CHECK((lower_bound_v<TT<char, short, int>, short,  comparator> == 1));
-    STATIC_CHECK((lower_bound_v<TT<char, short, int>, int,    comparator> == 2));
-    STATIC_CHECK((lower_bound_v<TT<char, short, int>, double, comparator> == 3));
-    STATIC_CHECK((lower_bound_v<TT<char, char, short, short, int, int>, char,  comparator> == 0));
-    STATIC_CHECK((lower_bound_v<TT<char, char, short, short, int, int>, short, comparator> == 2));
-    STATIC_CHECK((lower_bound_v<TT<char, char, short, short, int, int>, int,   comparator> == 4));
+    STATIC_CHECK((lower_bound_v<TT<>,                                   int,    comparator> == 0));
+    STATIC_CHECK((lower_bound_v<TT<int>,                                int,    comparator> == 0));
+    STATIC_CHECK((lower_bound_v<TT<char>,                               int,    comparator> == 1));
+    STATIC_CHECK((lower_bound_v<TT<char, short, int>,                   char,   comparator> == 0));
+    STATIC_CHECK((lower_bound_v<TT<char, short, int>,                   short,  comparator> == 1));
+    STATIC_CHECK((lower_bound_v<TT<char, short, int>,                   int,    comparator> == 2));
+    STATIC_CHECK((lower_bound_v<TT<char, short, int>,                   double, comparator> == 3));
+    STATIC_CHECK((lower_bound_v<TT<char, char, short, short, int, int>, char,   comparator> == 0));
+    STATIC_CHECK((lower_bound_v<TT<char, char, short, short, int, int>, short,  comparator> == 2));
+    STATIC_CHECK((lower_bound_v<TT<char, char, short, short, int, int>, int,    comparator> == 4));
   }
 
   template<template<class...> class TT>
   void type_algorithms_free_test::test_filter()
   {   
-    STATIC_CHECK((std::is_same_v<filter_t<std::tuple<>, std::index_sequence<>>, std::tuple<>>));
-    STATIC_CHECK((std::is_same_v<filter_t<std::tuple<int>, std::index_sequence<>>, std::tuple<>>));
-    STATIC_CHECK((std::is_same_v<filter_t<std::tuple<int>, std::index_sequence<0>>, std::tuple<int>>));
-    STATIC_CHECK((std::is_same_v<filter_t<std::tuple<int, float>, std::index_sequence<0>>, std::tuple<int>>));
-    STATIC_CHECK((std::is_same_v<filter_t<std::tuple<int, float>, std::index_sequence<1>>, std::tuple<float>>));
-    STATIC_CHECK((std::is_same_v<filter_t<std::tuple<int, float>, std::index_sequence<0, 1>>, std::tuple<int, float>>));
+    STATIC_CHECK((std::is_same_v<filter_t<TT<>,           std::index_sequence<>>,     TT<>>));
+    STATIC_CHECK((std::is_same_v<filter_t<TT<int>,        std::index_sequence<>>,     TT<>>));
+    STATIC_CHECK((std::is_same_v<filter_t<TT<int>,        std::index_sequence<0>>,    TT<int>>));
+    STATIC_CHECK((std::is_same_v<filter_t<TT<int, float>, std::index_sequence<0>>,    TT<int>>));
+    STATIC_CHECK((std::is_same_v<filter_t<TT<int, float>, std::index_sequence<1>>,    TT<float>>));
+    STATIC_CHECK((std::is_same_v<filter_t<TT<int, float>, std::index_sequence<0, 1>>, TT<int, float>>));
     
-    STATIC_CHECK((std::is_same_v<filter_by_trait_t<std::tuple<>, is_int>, std::tuple<>>));
-    STATIC_CHECK((std::is_same_v<filter_by_trait_t<std::tuple<float>, is_int>, std::tuple<>>));
-    STATIC_CHECK((std::is_same_v<filter_by_trait_t<std::tuple<int>, is_int>, std::tuple<int>>));
-    STATIC_CHECK((std::is_same_v<filter_by_trait_t<std::tuple<char, int>, is_int>, std::tuple<int>>));
-    STATIC_CHECK((std::is_same_v<filter_by_trait_t<std::tuple<int, char>, is_int>, std::tuple<int>>));
-    STATIC_CHECK((std::is_same_v<filter_by_trait_t<std::tuple<int, int>, is_int>, std::tuple<int, int>>));
+    STATIC_CHECK((std::is_same_v<filter_by_trait_t<TT<>,          is_int>, TT<>>));
+    STATIC_CHECK((std::is_same_v<filter_by_trait_t<TT<float>,     is_int>, TT<>>));
+    STATIC_CHECK((std::is_same_v<filter_by_trait_t<TT<int>,       is_int>, TT<int>>));
+    STATIC_CHECK((std::is_same_v<filter_by_trait_t<TT<char, int>, is_int>, TT<int>>));
+    STATIC_CHECK((std::is_same_v<filter_by_trait_t<TT<int, char>, is_int>, TT<int>>));
+    STATIC_CHECK((std::is_same_v<filter_by_trait_t<TT<int, int>,  is_int>, TT<int, int>>));
   }
 
   template<template<class...> class TT>
   void type_algorithms_free_test::test_drop()
   {
-    STATIC_CHECK((std::is_same_v<drop_t<std::tuple<>, 0>, std::tuple<>>));
-    STATIC_CHECK((std::is_same_v<drop_t<std::tuple<int>, 0>, std::tuple<int>>));
-    STATIC_CHECK((std::is_same_v<drop_t<std::tuple<int>, 1>, std::tuple<>>));
-    STATIC_CHECK((std::is_same_v<drop_t<std::tuple<char, int>, 1>, std::tuple<int>>));
-    STATIC_CHECK((std::is_same_v<drop_t<std::tuple<char, int>, 2>, std::tuple<>>));
+    STATIC_CHECK((std::is_same_v<drop_t<TT<>, 0>,          TT<>>));
+    STATIC_CHECK((std::is_same_v<drop_t<TT<int>, 0>,       TT<int>>));
+    STATIC_CHECK((std::is_same_v<drop_t<TT<int>, 1>,       TT<>>));
+    STATIC_CHECK((std::is_same_v<drop_t<TT<char, int>, 1>, TT<int>>));
+    STATIC_CHECK((std::is_same_v<drop_t<TT<char, int>, 2>, TT<>>));
   }
 
   template<template<class...> class TT>
   void type_algorithms_free_test::test_keep()
   {
-    STATIC_CHECK((std::is_same_v<keep_t<std::tuple<>, 0>, std::tuple<>>));
-    STATIC_CHECK((std::is_same_v<keep_t<std::tuple<int>, 0>, std::tuple<>>));
-    STATIC_CHECK((std::is_same_v<keep_t<std::tuple<int>, 1>, std::tuple<int>>));
-    STATIC_CHECK((std::is_same_v<keep_t<std::tuple<char, int>, 0>, std::tuple<>>));
-    STATIC_CHECK((std::is_same_v<keep_t<std::tuple<char, int>, 1>, std::tuple<char>>));
-    STATIC_CHECK((std::is_same_v<keep_t<std::tuple<char, int>, 2>, std::tuple<char, int>>));
+    STATIC_CHECK((std::is_same_v<keep_t<TT<>, 0>,          TT<>>));
+    STATIC_CHECK((std::is_same_v<keep_t<TT<int>, 0>,       TT<>>));
+    STATIC_CHECK((std::is_same_v<keep_t<TT<int>, 1>,       TT<int>>));
+    STATIC_CHECK((std::is_same_v<keep_t<TT<char, int>, 0>, TT<>>));
+    STATIC_CHECK((std::is_same_v<keep_t<TT<char, int>, 1>, TT<char>>));
+    STATIC_CHECK((std::is_same_v<keep_t<TT<char, int>, 2>, TT<char, int>>));
   }
 
   template<template<class...> class TT>
   void type_algorithms_free_test::test_merge()
   {
-    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<>, std::tuple<>, comparator>, std::tuple<>>));
-    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<>, std::tuple<int>, comparator>, std::tuple<int>>));
-    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<int>, std::tuple<>, comparator>, std::tuple<int>>));
-    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<int>, std::tuple<int>,  comparator>, std::tuple<int, int>>));
-    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<char>, std::tuple<int>, comparator>, std::tuple<char, int>>));
-    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<int>, std::tuple<char>, comparator>, std::tuple<char, int>>));
-    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<char>, std::tuple<char, int>, comparator>, std::tuple<char, char, int>>));
-    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<short>, std::tuple<char, int>, comparator>, std::tuple<char, short, int>>));
-    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<double>, std::tuple<char, int>, comparator>, std::tuple<char, int, double>>));
-    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<char, int>, std::tuple<char>, comparator>, std::tuple<char, char, int>>));
-    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<char, int>, std::tuple<char, short>, comparator>, std::tuple<char, char, short, int>>));
-    STATIC_CHECK((std::is_same_v<merge_t<std::tuple<short, int>, std::tuple<char, short, double>, comparator>, std::tuple<char, short, short, int, double>>));
+    STATIC_CHECK((std::is_same_v<merge_t<TT<>,           TT<>,                    comparator>, TT<>>));
+    STATIC_CHECK((std::is_same_v<merge_t<TT<>,           TT<int>,                 comparator>, TT<int>>));
+    STATIC_CHECK((std::is_same_v<merge_t<TT<int>,        TT<>,                    comparator>, TT<int>>));
+    STATIC_CHECK((std::is_same_v<merge_t<TT<int>,        TT<int>,                 comparator>, TT<int, int>>));
+    STATIC_CHECK((std::is_same_v<merge_t<TT<char>,       TT<int>,                 comparator>, TT<char, int>>));
+    STATIC_CHECK((std::is_same_v<merge_t<TT<int>,        TT<char>,                comparator>, TT<char, int>>));
+    STATIC_CHECK((std::is_same_v<merge_t<TT<char>,       TT<char, int>,           comparator>, TT<char, char, int>>));
+    STATIC_CHECK((std::is_same_v<merge_t<TT<short>,      TT<char, int>,           comparator>, TT<char, short, int>>));
+    STATIC_CHECK((std::is_same_v<merge_t<TT<double>,     TT<char, int>,           comparator>, TT<char, int, double>>));
+    STATIC_CHECK((std::is_same_v<merge_t<TT<char, int>,  TT<char>,                comparator>, TT<char, char, int>>));
+    STATIC_CHECK((std::is_same_v<merge_t<TT<char, int>,  TT<char, short>,         comparator>, TT<char, char, short, int>>));
+    STATIC_CHECK((std::is_same_v<merge_t<TT<short, int>, TT<char, short, double>, comparator>, TT<char, short, short, int, double>>));
   }
 
   template<template<class...> class TT>
   void type_algorithms_free_test::test_stable_sort()
   {
-    STATIC_CHECK((std::is_same_v<stable_sort_t<std::tuple<>, comparator>, std::tuple<>>));
-    STATIC_CHECK((std::is_same_v<stable_sort_t<std::tuple<int>, comparator>, std::tuple<int>>));
-    STATIC_CHECK((std::is_same_v<stable_sort_t<std::tuple<char, int>, comparator>, std::tuple<char, int>>));
-    STATIC_CHECK((std::is_same_v<stable_sort_t<std::tuple<int, char>, comparator>, std::tuple<char, int>>));
-    STATIC_CHECK((std::is_same_v<stable_sort_t<std::tuple<int, char, double, short>, comparator>, std::tuple<char, short, int, double>>));
+    STATIC_CHECK((std::is_same_v<stable_sort_t<TT<>,                         comparator>, TT<>>));
+    STATIC_CHECK((std::is_same_v<stable_sort_t<TT<int>,                      comparator>, TT<int>>));
+    STATIC_CHECK((std::is_same_v<stable_sort_t<TT<char, int>,                comparator>, TT<char, int>>));
+    STATIC_CHECK((std::is_same_v<stable_sort_t<TT<int, char>,                comparator>, TT<char, int>>));
+    STATIC_CHECK((std::is_same_v<stable_sort_t<TT<int, char, double, short>, comparator>, TT<char, short, int, double>>));
   }
 
   template<template<class...> class TT>
   void type_algorithms_free_test::test_find()
   {
-    STATIC_CHECK((find_v<std::tuple<>,                    int>    == 0));
-    STATIC_CHECK((find_v<std::tuple<int>,                 int>    == 0));
-    STATIC_CHECK((find_v<std::tuple<int>,                 float>  == 1));
-    STATIC_CHECK((find_v<std::tuple<int, int>,            int>    == 0));
-    STATIC_CHECK((find_v<std::tuple<int, int>,            float>  == 2));
-    STATIC_CHECK((find_v<std::tuple<int, float>,          float>  == 1));
-    STATIC_CHECK((find_v<std::tuple<int, double, float>,  int>    == 0));
-    STATIC_CHECK((find_v<std::tuple<int, double, float>,  double> == 1));
-    STATIC_CHECK((find_v<std::tuple<int, double, float>,  float>  == 2));
-    STATIC_CHECK((find_v<std::tuple<int, double, float>,  char>   == 3));
-    STATIC_CHECK((find_v<std::tuple<int, double, int>,    int>    == 0));
+    STATIC_CHECK((find_v<TT<>,                    int>    == 0));
+    STATIC_CHECK((find_v<TT<int>,                 int>    == 0));
+    STATIC_CHECK((find_v<TT<int>,                 float>  == 1));
+    STATIC_CHECK((find_v<TT<int, int>,            int>    == 0));
+    STATIC_CHECK((find_v<TT<int, int>,            float>  == 2));
+    STATIC_CHECK((find_v<TT<int, float>,          float>  == 1));
+    STATIC_CHECK((find_v<TT<int, double, float>,  int>    == 0));
+    STATIC_CHECK((find_v<TT<int, double, float>,  double> == 1));
+    STATIC_CHECK((find_v<TT<int, double, float>,  float>  == 2));
+    STATIC_CHECK((find_v<TT<int, double, float>,  char>   == 3));
+    STATIC_CHECK((find_v<TT<int, double, int>,    int>    == 0));
   }
 
   template<template<class...> class TT>
   void type_algorithms_free_test::test_find_if()
   {
-    STATIC_CHECK((find_if_v<std::tuple<>,                    std::is_integral>       == 0));
-    STATIC_CHECK((find_if_v<std::tuple<int>,                 std::is_integral>       == 0));
-    STATIC_CHECK((find_if_v<std::tuple<int>,                 std::is_floating_point> == 1));
-    STATIC_CHECK((find_if_v<std::tuple<int, int>,            std::is_integral>       == 0));
-    STATIC_CHECK((find_if_v<std::tuple<int, int>,            std::is_floating_point> == 2));
-    STATIC_CHECK((find_if_v<std::tuple<int, float>,          std::is_floating_point> == 1));
-    STATIC_CHECK((find_if_v<std::tuple<int, double, float>,  std::is_integral>       == 0));
-    STATIC_CHECK((find_if_v<std::tuple<int, double, float>,  std::is_floating_point> == 1));
-    STATIC_CHECK((find_if_v<std::tuple<float, double, int>,  std::is_integral>       == 2));
-    STATIC_CHECK((find_if_v<std::tuple<int, double, float>,  std::is_pointer>        == 3));
-    STATIC_CHECK((find_if_v<std::tuple<int, double, int>,    std::is_integral>       == 0));
+    STATIC_CHECK((find_if_v<TT<>,                    std::is_integral>       == 0));
+    STATIC_CHECK((find_if_v<TT<int>,                 std::is_integral>       == 0));
+    STATIC_CHECK((find_if_v<TT<int>,                 std::is_floating_point> == 1));
+    STATIC_CHECK((find_if_v<TT<int, int>,            std::is_integral>       == 0));
+    STATIC_CHECK((find_if_v<TT<int, int>,            std::is_floating_point> == 2));
+    STATIC_CHECK((find_if_v<TT<int, float>,          std::is_floating_point> == 1));
+    STATIC_CHECK((find_if_v<TT<int, double, float>,  std::is_integral>       == 0));
+    STATIC_CHECK((find_if_v<TT<int, double, float>,  std::is_floating_point> == 1));
+    STATIC_CHECK((find_if_v<TT<float, double, int>,  std::is_integral>       == 2));
+    STATIC_CHECK((find_if_v<TT<int, double, float>,  std::is_pointer>        == 3));
+    STATIC_CHECK((find_if_v<TT<int, double, int>,    std::is_integral>       == 0));
   }
 
   template<template<class...> class TT>
@@ -214,25 +214,25 @@ namespace sequoia::testing
   template<template<class...> class TT>
   void type_algorithms_free_test::test_erase()
   {
-    STATIC_CHECK((std::is_same_v<erase_t<std::tuple<int>, 0>,                std::tuple<>>));
-    STATIC_CHECK((std::is_same_v<erase_t<std::tuple<int, float>, 0>,         std::tuple<float>>));
-    STATIC_CHECK((std::is_same_v<erase_t<std::tuple<int, float>, 1>,         std::tuple<int>>));
-    STATIC_CHECK((std::is_same_v<erase_t<std::tuple<int, float, double>, 0>, std::tuple<float, double>>));
-    STATIC_CHECK((std::is_same_v<erase_t<std::tuple<int, float, double>, 1>, std::tuple<int, double>>));
-    STATIC_CHECK((std::is_same_v<erase_t<std::tuple<int, float, double>, 2>, std::tuple<int, float>>));
+    STATIC_CHECK((std::is_same_v<erase_t<TT<int>, 0>,                TT<>>));
+    STATIC_CHECK((std::is_same_v<erase_t<TT<int, float>, 0>,         TT<float>>));
+    STATIC_CHECK((std::is_same_v<erase_t<TT<int, float>, 1>,         TT<int>>));
+    STATIC_CHECK((std::is_same_v<erase_t<TT<int, float, double>, 0>, TT<float, double>>));
+    STATIC_CHECK((std::is_same_v<erase_t<TT<int, float, double>, 1>, TT<int, double>>));
+    STATIC_CHECK((std::is_same_v<erase_t<TT<int, float, double>, 2>, TT<int, float>>));
   }
 
   template<template<class...> class TT>
   void type_algorithms_free_test::test_insert()
   {
-    STATIC_CHECK((std::is_same_v<insert_t<std::tuple<>, int, 0>,            std::tuple<int>>));
-    STATIC_CHECK((std::is_same_v<insert_t<std::tuple<int>, int, 0>,         std::tuple<int, int>>));
-    STATIC_CHECK((std::is_same_v<insert_t<std::tuple<int>, int, 1>,         std::tuple<int, int>>));
-    STATIC_CHECK((std::is_same_v<insert_t<std::tuple<int>, float, 0>,       std::tuple<float, int>>));
-    STATIC_CHECK((std::is_same_v<insert_t<std::tuple<int>, float, 1>,       std::tuple<int, float>>));
-    STATIC_CHECK((std::is_same_v<insert_t<std::tuple<int, float>, char, 0>, std::tuple<char, int, float>>));
-    STATIC_CHECK((std::is_same_v<insert_t<std::tuple<int, float>, char, 1>, std::tuple<int, char, float>>));
-    STATIC_CHECK((std::is_same_v<insert_t<std::tuple<int, float>, char, 2>, std::tuple<int, float, char>>));
+    STATIC_CHECK((std::is_same_v<insert_t<TT<>, int, 0>,            TT<int>>));
+    STATIC_CHECK((std::is_same_v<insert_t<TT<int>, int, 0>,         TT<int, int>>));
+    STATIC_CHECK((std::is_same_v<insert_t<TT<int>, int, 1>,         TT<int, int>>));
+    STATIC_CHECK((std::is_same_v<insert_t<TT<int>, float, 0>,       TT<float, int>>));
+    STATIC_CHECK((std::is_same_v<insert_t<TT<int>, float, 1>,       TT<int, float>>));
+    STATIC_CHECK((std::is_same_v<insert_t<TT<int, float>, char, 0>, TT<char, int, float>>));
+    STATIC_CHECK((std::is_same_v<insert_t<TT<int, float>, char, 1>, TT<int, char, float>>));
+    STATIC_CHECK((std::is_same_v<insert_t<TT<int, float>, char, 2>, TT<int, float, char>>));
   }
 
   template<template<class...> class TT>
